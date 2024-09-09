7b2938786f8e3032e8454a9de76ed3fd44430107
Christopher Bignamini <christopher.bignamini@gmail.com>
2024-09-09 17:29:08 +0200

    halos_utility header added

--- ryoanji/test/interface/global_upsweep_cpu.cpp
+++ ryoanji/test/interface/global_upsweep_cpu.cpp
@@ -34,6 +34,7 @@
 
 #include "cstone/domain/domain.hpp"
 #include "cstone/findneighbors.hpp"
+#include "cstone/halos/halos_utility.hpp"
 #include "coord_samples/random.hpp"
 
 #include "ryoanji/interface/global_multipole.hpp"
