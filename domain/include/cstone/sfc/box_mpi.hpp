1deede90e4a8f52de40b5963358d1d64c893750e
nknk567 <noah.kubli@uzh.ch>
2024-12-06 17:36:06 +0100

    Prevent fast bounding box changes when removing single particles

--- domain/include/cstone/sfc/box_mpi.hpp
+++ domain/include/cstone/sfc/box_mpi.hpp
@@ -27,6 +27,7 @@
  * @brief compute global minima and maxima of array ranges
  *
  * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ * @author Noah Kubli <noah.kubli@uzh.ch>
  *
  */
 
@@ -106,15 +107,19 @@ auto makeGlobalBox(const T* x, const T* y, const T* z, size_t numElements, const
         extrema[5] = -extrema[5];
     }
 
-    return Box<T>{extrema[0],
-                  extrema[1],
-                  extrema[2],
-                  extrema[3],
-                  extrema[4],
-                  extrema[5],
-                  previousBox.boundaryX(),
-                  previousBox.boundaryY(),
-                  previousBox.boundaryZ()};
+    // Limit shrinking of the bounding box in a single time-step due to removing of single particles that are far from
+    // the system.
+    // The limit is applied equally on both sides such that the box can still follow the center of mass.
+    constexpr T shrink_limit{0.9};
+    const cstone::Vec3<T> previousDiff{previousBox.xmax() - previousBox.xmin(), previousBox.ymax() - previousBox.ymin(),
+                                       previousBox.zmax() - previousBox.zmin()};
+    const cstone::Vec3<T> tightFitDiff{extrema[1] - extrema[0], extrema[3] - extrema[2], extrema[5] - extrema[4]};
+    const auto diffLowerLimit = previousDiff * shrink_limit;
+    const auto diffAdd        = max((diffLowerLimit - tightFitDiff) * 0.5, cstone::Vec3<T>{0., 0., 0.});
+
+    return Box<T>{extrema[0] - diffAdd[0], extrema[1] + diffAdd[0], extrema[2] - diffAdd[1],
+                  extrema[3] + diffAdd[1], extrema[4] - diffAdd[2], extrema[5] + diffAdd[2],
+                  previousBox.boundaryX(), previousBox.boundaryY(), previousBox.boundaryZ()};
 }
 
 } // namespace cstone
