6795a6e366642ce86497a1f0f9212571070a3db3
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-02-07 08:48:11 +0100

    Particle selection from id list fix and selection from sphere added

--- main/src/sphexa/particle_selection.hpp
+++ main/src/sphexa/particle_selection.hpp
@@ -15,16 +15,80 @@ namespace sphexa
 using ParticleIdType = uint64_t; // TODO: retrieve type from ParticlesData
 constexpr ParticleIdType msbMask = static_cast<ParticleIdType>(1) << (sizeof(ParticleIdType)*8 - 1);
 
-extern void findSelectedParticlesIndexes_gpu(const ParticlesData<cstone::GpuTag>& d, size_t first, size_t last, 
-                                             std::vector<uint64_t>& localSelectedParticlesIndexes);
+struct ParticleSelectionSphere
+{
+    std::array<double, 3> center;
+    double radius;
+};
+
+// Find particles in user provided sphere and tag them
+extern void findParticlesInSphere_gpu(ParticlesData<cstone::GpuTag>& d, size_t firstIndex, size_t lastIndex, const ParticleSelectionSphere& selSphereData);
 
 template<class AccType>
-void findSelectedParticlesIndexes(const ParticlesData<AccType>& d, size_t first, size_t last, 
-                                  std::vector<uint64_t>& localSelectedParticlesIndexes)
+void findParticlesInSphere(ParticlesData<AccType>& d, size_t firstIndex, size_t lastIndex, const ParticleSelectionSphere& selSphereData) {
+
+    if constexpr (cstone::HaveGpu<AccType>{})
+    {
+        findParticlesInSphere_gpu(d, firstIndex, lastIndex, selSphereData);
+    }
+    else
+    {
+        // TODO: can we use C++23 zip iterators? Is there anything already implemented in SPH-EXA?
+        const auto squareRadius = selSphereData.radius*selSphereData.radius;
+//#pragma omp parallel for
+        for(auto particleIndex = firstIndex; particleIndex < lastIndex; particleIndex++){
+            auto x = d.x[particleIndex];
+            auto y = d.y[particleIndex];
+            auto z = d.z[particleIndex];
+            // std::cout<<particleIndex<<" "<<x<<" "<<y<<" "<<z<<" "<<(x - selSphereData.center[0])*(x - selSphereData.center[0]) +
+            //    (y - selSphereData.center[1])*(y - selSphereData.center[1]) +
+            //    (z - selSphereData.center[2])*(z - selSphereData.center[2])<<" "<<squareRadius<<std::endl;
+            if((x - selSphereData.center[0])*(x - selSphereData.center[0]) +
+               (y - selSphereData.center[1])*(y - selSphereData.center[1]) +
+               (z - selSphereData.center[2])*(z - selSphereData.center[2]) <= squareRadius) {
+                // std::cout<<" Tag particle "<<std::endl;
+                d.id[particleIndex] = d.id[particleIndex] | msbMask;
+            }
+        }
+    }
+}
+
+
+// Find particles in user provided id list and tag them
+// TODO: only pass d.id
+extern void findParticlesInIdList_gpu(ParticlesData<cstone::GpuTag>& d, const std::vector<ParticleIdType>& selParticlesIds);
+
+template<class AccType>
+void findParticlesInIdList(ParticlesData<AccType>& d, size_t firstIndex, size_t lastIndex, const std::vector<ParticleIdType>& selParticlesIds) {
+
+    if constexpr (cstone::HaveGpu<AccType>{})
+    {
+        findParticlesInIdList_gpu(d, firstIndex, lastIndex, selParticlesIds);
+    }
+    else
+    {
+        const auto idListBeginIt = d.id.begin()+firstIndex;
+        const auto idListEndIt = d.id.begin()+lastIndex;
+        std::for_each(selParticlesIds.begin(), selParticlesIds.end(), [idListBeginIt, idListEndIt](auto selParticleId){
+            // Find the selected particles in user provided id list and tag them by setting the MSB of the id field
+            auto selParticleIt = std::find(idListBeginIt, idListEndIt, selParticleId);
+            if (selParticleIt != idListEndIt) {
+                *selParticleIt = *selParticleIt | msbMask;
+            }
+        });
+    }
+}
+
+// Identify tagged particles and save their indexes
+extern void findSelectedParticlesIndexes_gpu(const ParticlesData<cstone::GpuTag>& d, std::vector<uint64_t>& localSelectedParticlesIndexes);
+
+// TODO: search in size_t firstIndex, size_t lastIndex
+template<class AccType>
+void findSelectedParticlesIndexes(const ParticlesData<AccType>& d, std::vector<uint64_t>& localSelectedParticlesIndexes)
 {
     if constexpr (cstone::HaveGpu<AccType>{})
     {
-        findSelectedParticlesIndexes_gpu(d, first, last, localSelectedParticlesIndexes);
+        findSelectedParticlesIndexes_gpu(d, localSelectedParticlesIndexes);
     }
     else
     {
