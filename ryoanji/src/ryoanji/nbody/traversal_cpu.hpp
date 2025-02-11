2eea9e7d009419224b66bc80c256caf79d6bf72b
Sebastian Keller <sebastian.f.keller@gmail.com>
2025-02-11 20:59:25 +0100

    improve variable naming

--- ryoanji/src/ryoanji/nbody/traversal_cpu.hpp
+++ ryoanji/src/ryoanji/nbody/traversal_cpu.hpp
@@ -104,15 +104,15 @@ void computeGravityGroup(const util::array<Vec4<T1>, N>& target, const TreeNodeI
 
         bool violatesMac = cstone::evaluateMac(makeVec3(com), com[3], targetCenter, targetSize);
 
-        auto lidx = internalToLeaf[idx];
+        auto leafIdx = internalToLeaf[idx];
         // A leaf cell with non-zero multipole mass, but no particles must be remote.
         // Domain.syncGrav guarantees that such a cell never fails MAC. We may still see a MAC failure here
         // because target group particle bounding boxes may protrude outside the assigned SFC domain.
         // In this case we may nevertheless apply M2P because the protruding part that caused the mac to fail
         // must have contained no particles, because particles can only be inside the SFC domain.
-        bool isRemoteUntaggedLeaf = lidx >= 0 && layout[lidx] == layout[lidx + 1] && mp[Cqi::mass] > 0;
+        bool isRemote = leafIdx >= 0 && layout[leafIdx] == layout[leafIdx + 1] && mp[Cqi::mass] > 0;
 
-        if (!violatesMac || isRemoteUntaggedLeaf)
+        if (!violatesMac || isRemote)
         {
 // apply multipole to all particles in group
 #if defined(__llvm__) || defined(__clang__)
