4b70c7677b8c04701ba48551e25ef5525a10727d
Felix Thaler <thaler@cscs.ch>
2025-03-12 12:50:01 +0100

    Port VeDefGradh kernel to neighborhood

--- sph/include/sph/hydro_ve/ve_def_gradh.hpp
+++ sph/include/sph/hydro_ve/ve_def_gradh.hpp
@@ -36,52 +36,16 @@
 
 namespace sph
 {
-template<class Tc, class Dataset>
-void computeVeDefGradhImpl(size_t startIndex, size_t endIndex, Dataset& d, const cstone::Box<Tc>& box)
-{
-    const cstone::LocalIndex* neighbors      = d.neighbors.data();
-    const unsigned*           neighborsCount = d.nc.data();
-
-    const auto* x = d.x.data();
-    const auto* y = d.y.data();
-    const auto* z = d.z.data();
-    const auto* h = d.h.data();
-    const auto* m = d.m.data();
-
-    const auto* wh  = d.wh.data();
-    const auto* whd = d.whd.data();
-
-    const auto* xm = d.xm.data();
-
-    auto* kx    = d.kx.data();
-    auto* gradh = d.gradh.data();
-
-    const Tc K         = d.K;
-    const Tc sincIndex = d.sincIndex;
-
-#pragma omp parallel for
-    for (size_t i = startIndex; i < endIndex; i++)
-    {
-        size_t   ni        = i - startIndex;
-        unsigned ncCapped  = std::min(neighborsCount[i] - 1, d.ngmax);
-        auto [kxi, gradhi] = veDefGradhJLoop(i, K, box, neighbors + d.ngmax * ni, ncCapped, x, y, z, h, m, wh, whd, xm);
-
-        kx[i]    = kxi;
-        gradh[i] = gradhi;
-
-#ifndef NDEBUG
-        auto rhoi = kxi * m[i] / xm[i];
-        if (std::isnan(rhoi))
-            printf("ERROR::Density(%zu) density %f, position: (%f %f %f), h: %f\n", i, rhoi, x[i], y[i], z[i], h[i]);
-#endif
-    }
-}
 
 template<typename Tc, class Dataset>
 void computeVeDefGradh(const GroupView& grp, Dataset& d, const cstone::Box<Tc>& box)
 {
     if constexpr (cstone::HaveGpu<typename Dataset::AcceleratorType>{}) { cuda::computeVeDefGradh(grp, d, box); }
-    else { computeVeDefGradhImpl(grp.firstBody, grp.lastBody, d, box); }
+    else
+    {
+        veDefGradhIjLoop(getNeighborhood(d), d.K, d.m.data(), d.xm.data(), d.wh.data(), d.whd.data(), d.kx.data(),
+                         d.gradh.data());
+    }
 }
 
 } // namespace sph
--- sph/include/sph/hydro_ve/ve_def_gradh_kern.hpp
+++ sph/include/sph/hydro_ve/ve_def_gradh_kern.hpp
@@ -33,6 +33,7 @@
 
 #include "cstone/cuda/annotation.hpp"
 #include "cstone/sfc/box.hpp"
+#include "cstone/traversal/ijloop/ijloop.hpp"
 
 #include "sph/kernels.hpp"
 #include "sph/table_lookup.hpp"
@@ -40,53 +41,108 @@
 namespace sph
 {
 
+template<class T>
+struct VeDefGradHInteraction
+{
+    const T *wh, *whd;
+
+    template<class ParticleData, class Tc>
+    constexpr auto operator()(const ParticleData& iData, const ParticleData& jData, cstone::Vec3<Tc> const& /* r_ij */,
+                              T r2) const
+    {
+        const auto [i, iPos, hi, mi, xmassi] = iData;
+        const auto [j, jPos, hj, mj, xmassj] = jData;
+
+        auto hInv = T(1) / hi;
+
+        T dist  = std::sqrt(r2);
+        T vloc  = dist * hInv;
+        T w     = lt::lookup(wh, vloc);
+        T dw    = lt::lookup(whd, vloc);
+        T dterh = -(T(3) * w + vloc * dw);
+
+        T kxi      = w * xmassj;
+        T whomegai = dterh * xmassj;
+        T wrho0i   = dterh * mj;
+
+        return std::make_tuple(kxi, whomegai, wrho0i);
+    }
+};
+
+template<class T, class Tc>
+struct VeDefGradHPostamble
+{
+    Tc K;
+
+    template<class ParticleData, class Result>
+    constexpr auto operator()(const ParticleData& iData, const Result& result) const
+    {
+        const auto [i, iPos, hi, mi, xmassi] = iData;
+        auto [kxi, whomegai, wrho0i]         = result;
+
+        auto hInv  = T(1) / hi;
+        auto h3Inv = hInv * hInv * hInv;
+
+        kxi *= K * h3Inv;
+        whomegai *= K * h3Inv * hInv;
+        wrho0i *= K * h3Inv * hInv;
+
+        whomegai = whomegai * mi / xmassi + (kxi - K * xmassi * h3Inv) * wrho0i;
+        T rhoi   = kxi * mi / xmassi;
+        T dhdrho = -hi / (rhoi * T(3)); // This /3 is the dimension hard-coded.
+
+        T gradhi = T(1) - dhdrho * whomegai;
+#ifndef NDEBUG
+        if (std::isnan(rhoi))
+        {
+            printf("ERROR::Density(%zu) density %f, position: (%f %f %f), h: %f\n", size_t(i), rhoi, iPos[0], iPos[1],
+                   iPos[2], hi);
+        }
+#endif
+        return std::make_tuple(kxi, gradhi);
+    }
+};
+
 template<size_t stride = 1, class Tc, class Tm, class T>
 HOST_DEVICE_FUN inline util::tuple<T, T> veDefGradhJLoop(cstone::LocalIndex i, Tc K, const cstone::Box<Tc>& box,
                                                          const cstone::LocalIndex* neighbors, unsigned neighborsCount,
                                                          const Tc* x, const Tc* y, const Tc* z, const T* h, const Tm* m,
                                                          const T* wh, const T* whd, const T* xm)
 {
-    auto xi     = x[i];
-    auto yi     = y[i];
-    auto zi     = z[i];
-    auto hi     = h[i];
-    auto mi     = m[i];
-    auto xmassi = xm[i];
+    VeDefGradHInteraction      interaction{wh, whd};
+    VeDefGradHPostamble<T, Tc> postamble{K};
 
-    auto hInv  = T(1) / hi;
-    auto h3Inv = hInv * hInv * hInv;
+    const auto input = std::make_tuple(m, xm);
+    T          kxi, gradhi;
+    const auto output = std::make_tuple(&kxi - i, &gradhi - i);
 
-    // initialize with self-contribution
-    auto kxi      = xmassi;
-    auto whomegai = -T(3) * xmassi;
-    auto wrho0i   = -T(3) * mi;
+    const auto iData  = cstone::ijloop::loadParticleData(x, y, z, h, input, i);
+    const bool usePbc = cstone::ijloop::requiresPbcHandling(box, iData);
 
+    auto result = interaction(iData, iData, cstone::Vec3<Tc>{0, 0, 0}, T(0));
     for (unsigned pj = 0; pj < neighborsCount; ++pj)
     {
         cstone::LocalIndex j = neighbors[stride * pj];
 
-        T dist   = distancePBC(box, hi, xi, yi, zi, x[j], y[j], z[j]);
-        T vloc   = dist * hInv;
-        T w      = lt::lookup(wh, vloc);
-        T dw     = lt::lookup(whd, vloc);
-        T dterh  = -(T(3) * w + vloc * dw);
-        T xmassj = xm[j];
+        const auto jData = cstone::ijloop::loadParticleData(x, y, z, h, input, j);
 
-        kxi += w * xmassj;
-        whomegai += dterh * xmassj;
-        wrho0i += dterh * m[j];
+        const auto [r_ij, r2] = cstone::ijloop::posDiffAndDistSq(usePbc, box, iData, jData);
+
+        cstone::ijloop::updateResult(result, interaction(iData, jData, r_ij, r2));
     }
 
-    kxi *= K * h3Inv;
-    whomegai *= K * h3Inv * hInv;
-    wrho0i *= K * h3Inv * hInv;
+    auto presult = postamble(iData, result);
 
-    whomegai = whomegai * mi / xmassi + (kxi - K * xmassi * h3Inv) * wrho0i;
-    T rhoi   = kxi * mi / xmassi;
-    T dhdrho = -hi / (rhoi * T(3)); // This /3 is the dimension hard-coded.
-
-    T gradhi = T(1) - dhdrho * whomegai;
+    cstone::ijloop::storeParticleData(output, i, presult);
     return {kxi, gradhi};
+}
+
+template<class Neighbordhood, class Tc, class Tm, class T>
+void veDefGradhIjLoop(const Neighbordhood& neighborhood, Tc K, const Tm* m, const T* xm, const T* wh, const T* whd,
+                      T* kx, T* gradh)
+{
+    neighborhood.ijLoop(std::make_tuple(m, xm), std::make_tuple(kx, gradh), VeDefGradHInteraction{wh, whd},
+                        VeDefGradHPostamble<T, Tc>{K}, cstone::ijloop::symmetry::asymmetric); // TODO: check symmetry
 }
 
 } // namespace sph
