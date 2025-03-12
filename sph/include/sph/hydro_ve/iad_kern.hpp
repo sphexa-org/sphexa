fe446e9a65f1dc6e4aa95dd52ee547e724fd065a
Felix Thaler <thaler@cscs.ch>
2025-03-12 10:49:53 +0100

    Implement ve IADJLoop as reduction + postamble

--- sph/include/sph/hydro_ve/iad_kern.hpp
+++ sph/include/sph/hydro_ve/iad_kern.hpp
@@ -33,6 +33,7 @@
 
 #include "cstone/cuda/annotation.hpp"
 #include "cstone/sfc/box.hpp"
+#include "cstone/traversal/ijloop/ijloop.hpp"
 
 #include "sph/kernels.hpp"
 #include "sph/table_lookup.hpp"
@@ -40,72 +41,110 @@
 namespace sph
 {
 
+template<class T>
+struct IADInteraction
+{
+    const T* wh;
+
+    template<class ParticleData, class Tc>
+    constexpr auto operator()(const ParticleData& iData, const ParticleData& jData, cstone::Vec3<Tc> const& r_ij,
+                              T r2) const
+    {
+        const auto [i, iPos, hi, xmi, kxi] = iData;
+        const auto [j, jPos, hj, xmj, kxj] = jData;
+
+        T rx = r_ij[0];
+        T ry = r_ij[1];
+        T rz = r_ij[2];
+
+        T hiInv = T(1) / hi;
+
+        T dist = std::sqrt(r2);
+
+        T vloc = dist * hiInv;
+        T w    = lt::lookup(wh, vloc);
+
+        T volj_w = xmj / kxj * w;
+
+        return std::make_tuple(rx * rx * volj_w, //
+                               rx * ry * volj_w, //
+                               rx * rz * volj_w, //
+                               ry * ry * volj_w, //
+                               ry * rz * volj_w, //
+                               rz * rz * volj_w);
+    }
+};
+
+template<class T, class Tc>
+struct IADPostamble
+{
+    Tc K;
+
+    template<class ParticleData, class Result>
+    constexpr auto operator()(const ParticleData& iData, const Result& result) const
+    {
+        const auto [i, iPos, hi, mi, roi]               = iData;
+        auto [tau11, tau12, tau13, tau22, tau23, tau33] = result;
+
+        auto getExp    = [](T val) { return (val == T(0) ? 0 : std::ilogb(val)); };
+        int  tauExpSum = getExp(tau11) + getExp(tau12) + getExp(tau13) + getExp(tau22) + getExp(tau23) + getExp(tau33);
+        // normalize with 2^-averageTauExponent, ldexp(a, b) == a * 2^b
+        T normalization = std::ldexp(T(1), -tauExpSum / 6);
+
+        tau11 *= normalization;
+        tau12 *= normalization;
+        tau13 *= normalization;
+        tau22 *= normalization;
+        tau23 *= normalization;
+        tau33 *= normalization;
+
+        T det = tau11 * tau22 * tau33 + T(2) * tau12 * tau23 * tau13 - tau11 * tau23 * tau23 - tau22 * tau13 * tau13 -
+                tau33 * tau12 * tau12;
+
+        // Note normalization factor: cij have units of 1/tau because det is proportional to tau^3 so we have to
+        // divide by K/h^3.
+        T factor = normalization * (hi * hi * hi) / (det * K);
+
+        return std::make_tuple(                       //
+            (tau22 * tau33 - tau23 * tau23) * factor, //
+            (tau13 * tau23 - tau33 * tau12) * factor, //
+            (tau12 * tau23 - tau22 * tau13) * factor, //
+            (tau11 * tau33 - tau13 * tau13) * factor, //
+            (tau13 * tau12 - tau11 * tau23) * factor, //
+            (tau11 * tau22 - tau12 * tau12) * factor);
+    }
+};
+
 template<size_t stride = 1, class Tc, class T>
 HOST_DEVICE_FUN inline void IADJLoop(cstone::LocalIndex i, Tc K, const cstone::Box<Tc>& box,
                                      const cstone::LocalIndex* neighbors, unsigned neighborsCount, const Tc* x,
                                      const Tc* y, const Tc* z, const T* h, const T* wh, const T* /*whd*/, const T* xm,
                                      const T* kx, T* c11, T* c12, T* c13, T* c22, T* c23, T* c33)
 {
-    T tau11 = 0.0, tau12 = 0.0, tau13 = 0.0, tau22 = 0.0, tau23 = 0.0, tau33 = 0.0;
+    IADInteraction      interaction{wh};
+    IADPostamble<T, Tc> postamble{K};
 
-    auto xi = x[i];
-    auto yi = y[i];
-    auto zi = z[i];
+    const auto input  = std::make_tuple(xm, kx);
+    const auto output = std::make_tuple(c11, c12, c13, c22, c23, c33);
 
-    auto hi    = h[i];
-    auto hiInv = T(1) / hi;
+    const auto iData  = cstone::ijloop::loadParticleData(x, y, z, h, input, i);
+    const bool usePbc = cstone::ijloop::requiresPbcHandling(box, iData);
 
+    auto result = interaction(iData, iData, cstone::Vec3<Tc>{0, 0, 0}, T(0));
     for (unsigned pj = 0; pj < neighborsCount; ++pj)
     {
         cstone::LocalIndex j = neighbors[stride * pj];
 
-        T rx = (xi - x[j]);
-        T ry = (yi - y[j]);
-        T rz = (zi - z[j]);
+        const auto jData = cstone::ijloop::loadParticleData(x, y, z, h, input, j);
 
-        applyPBC(box, T(2) * hi, rx, ry, rz);
+        const auto [r_ij, r2] = cstone::ijloop::posDiffAndDistSq(usePbc, box, iData, jData);
 
-        T dist = std::sqrt(rx * rx + ry * ry + rz * rz);
-
-        // calculate the v as ratio between the distance and the smoothing length
-        T vloc = dist * hiInv;
-        T w    = lt::lookup(wh, vloc);
-
-        T volj_w = xm[j] / kx[j] * w;
-
-        tau11 += rx * rx * volj_w;
-        tau12 += rx * ry * volj_w;
-        tau13 += rx * rz * volj_w;
-        tau22 += ry * ry * volj_w;
-        tau23 += ry * rz * volj_w;
-        tau33 += rz * rz * volj_w;
+        cstone::ijloop::updateResult(result, interaction(iData, jData, r_ij, r2));
     }
 
-    auto getExp    = [](T val) { return (val == T(0) ? 0 : std::ilogb(val)); };
-    int  tauExpSum = getExp(tau11) + getExp(tau12) + getExp(tau13) + getExp(tau22) + getExp(tau23) + getExp(tau33);
-    // normalize with 2^-averageTauExponent, ldexp(a, b) == a * 2^b
-    T normalization = std::ldexp(T(1), -tauExpSum / 6);
+    result = postamble(iData, result);
 
-    tau11 *= normalization;
-    tau12 *= normalization;
-    tau13 *= normalization;
-    tau22 *= normalization;
-    tau23 *= normalization;
-    tau33 *= normalization;
-
-    T det = tau11 * tau22 * tau33 + T(2) * tau12 * tau23 * tau13 - tau11 * tau23 * tau23 - tau22 * tau13 * tau13 -
-            tau33 * tau12 * tau12;
-
-    // note normalization factor: cij have units of 1/tau because det is proportional to tau^3, so we have to
-    // divide by K/h^3
-    T factor = normalization * (hi * hi * hi) / (det * K);
-
-    c11[i] = (tau22 * tau33 - tau23 * tau23) * factor;
-    c12[i] = (tau13 * tau23 - tau33 * tau12) * factor;
-    c13[i] = (tau12 * tau23 - tau22 * tau13) * factor;
-    c22[i] = (tau11 * tau33 - tau13 * tau13) * factor;
-    c23[i] = (tau13 * tau12 - tau11 * tau23) * factor;
-    c33[i] = (tau11 * tau22 - tau12 * tau12) * factor;
+    cstone::ijloop::storeParticleData(output, i, result);
 }
 
 } // namespace sph
