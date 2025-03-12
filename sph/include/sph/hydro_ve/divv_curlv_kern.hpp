4b9d463c57bf6b1cdbe35d646d8ca5fa303ca871
Felix Thaler <thaler@cscs.ch>
2025-03-12 11:19:33 +0100

    Implement divV_curlVJLoop as reduction + postamble

--- sph/include/sph/hydro_ve/divv_curlv_kern.hpp
+++ sph/include/sph/hydro_ve/divv_curlv_kern.hpp
@@ -33,6 +33,7 @@
 
 #include "cstone/cuda/annotation.hpp"
 #include "cstone/sfc/box.hpp"
+#include "cstone/traversal/ijloop/ijloop.hpp"
 
 #include "sph/kernels.hpp"
 #include "sph/table_lookup.hpp"
@@ -40,52 +41,28 @@
 namespace sph
 {
 
-template<size_t stride = 1, typename Tc, class T>
-HOST_DEVICE_FUN inline void
-divV_curlVJLoop(cstone::LocalIndex i, Tc K, const cstone::Box<Tc>& box, const cstone::LocalIndex* neighbors,
-                unsigned neighborsCount, const Tc* x, const Tc* y, const Tc* z, const T* vx, const T* vy, const T* vz,
-                const T* h, const T* c11, const T* c12, const T* c13, const T* c22, const T* c23, const T* c33,
-                const T* wh, const T* /*whd*/, const T* kx, const T* xm, T* divv, T* curlv, T* dV11, T* dV12, T* dV13,
-                T* dV22, T* dV23, T* dV33, bool doGradV)
+template<class T>
+struct DivVCurlVInteraction
 {
-    auto xi  = x[i];
-    auto yi  = y[i];
-    auto zi  = z[i];
-    auto vxi = vx[i];
-    auto vyi = vy[i];
-    auto vzi = vz[i];
-    auto hi  = h[i];
-    auto kxi = kx[i];
+    const T* wh;
 
-    auto hiInv  = T(1) / hi;
-    auto hiInv3 = hiInv * hiInv * hiInv;
-
-    // the 3 components of these vectors will be the derivatives in x,y,z directions
-    cstone::Vec3<T> dVxi{0., 0., 0.}, dVyi{0., 0., 0.}, dVzi{0., 0., 0.};
-
-    auto c11i = c11[i];
-    auto c12i = c12[i];
-    auto c13i = c13[i];
-    auto c22i = c22[i];
-    auto c23i = c23[i];
-    auto c33i = c33[i];
-
-    for (unsigned pj = 0; pj < neighborsCount; ++pj)
+    template<class ParticleData, class Tc>
+    constexpr auto operator()(const ParticleData& iData, const ParticleData& jData, const cstone::Vec3<Tc>& r_ij,
+                              const T r2) const
     {
-        cstone::LocalIndex j = neighbors[stride * pj];
+        auto const [i, iPos, hi, vxi, vyi, vzi, xmi, kxi, c11i, c12i, c13i, c22i, c23i, c33i] = iData;
+        auto const [j, jPos, hj, vxj, vyj, vzj, xmj, kxj, c11j, c12j, c13j, c22j, c23j, c33j] = jData;
 
-        T rx = xi - x[j];
-        T ry = yi - y[j];
-        T rz = zi - z[j];
+        T hiInv  = T(1) / hi;
 
-        applyPBC(box, T(2) * hi, rx, ry, rz);
-
-        T r2   = rx * rx + ry * ry + rz * rz;
+        T rx   = r_ij[0];
+        T ry   = r_ij[1];
+        T rz   = r_ij[2];
         T dist = std::sqrt(r2);
 
-        T vx_ji = vx[j] - vxi;
-        T vy_ji = vy[j] - vyi;
-        T vz_ji = vz[j] - vzi;
+        T vx_ji = vxj - vxi;
+        T vy_ji = vyj - vyi;
+        T vz_ji = vzj - vzi;
 
         T v1 = dist * hiInv;
         T Wi = lt::lookup(wh, v1);
@@ -95,30 +72,102 @@ divV_curlVJLoop(cstone::LocalIndex i, Tc K, const cstone::Box<Tc>& box, const cs
         termA[1] = -(c12i * rx + c22i * ry + c23i * rz) * Wi;
         termA[2] = -(c13i * rx + c23i * ry + c33i * rz) * Wi;
 
-        T xmassj = xm[j];
-
-        dVxi += (vx_ji * xmassj) * termA;
-        dVyi += (vy_ji * xmassj) * termA;
-        dVzi += (vz_ji * xmassj) * termA;
+        return std::make_tuple((vx_ji * xmj) * termA, (vy_ji * xmj) * termA, (vz_ji * xmj) * termA);
     }
+};
 
-    T norm_kxi = K * hiInv3 / kxi;
-    divv[i]    = norm_kxi * (dVxi[0] + dVyi[1] + dVzi[2]);
+template<bool DoCurlV, bool DoGradV, class T, class Tc>
+struct DivVCurlVPostamble
+{
+    Tc K;
 
-    if (curlv != nullptr)
+    template<class ParticleData, class Result>
+    constexpr auto operator()(const ParticleData& iData, const Result& result) const
     {
+        auto const [i, iPos, hi, vxi, vyi, vzi, xmi, kxi, c11i, c12i, c13i, c22i, c23i, c33i] = iData;
+        const auto [dVxi, dVyi, dVzi]                                                         = result;
+
+        T hiInv  = T(1) / hi;
+        T hiInv3 = hiInv * hiInv * hiInv;
+
+        T norm_kxi = K * hiInv3 / kxi;
+        T divvi    = norm_kxi * (dVxi[0] + dVyi[1] + dVzi[2]);
+
         cstone::Vec3<T> curlV{dVzi[1] - dVyi[2], dVxi[2] - dVzi[0], dVyi[0] - dVxi[1]};
-        curlv[i] = norm_kxi * std::sqrt(norm2(curlV));
+        T               curlvi = norm_kxi * std::sqrt(norm2(curlV));
+
+        T dV11i = norm_kxi * dVxi[0];
+        T dV12i = norm_kxi * (dVxi[1] + dVyi[0]);
+        T dV13i = norm_kxi * (dVxi[2] + dVzi[0]);
+        T dV22i = norm_kxi * dVyi[1];
+        T dV23i = norm_kxi * (dVyi[2] + dVzi[1]);
+        T dV33i = norm_kxi * dVzi[2];
+
+        if constexpr (DoCurlV && DoGradV)
+            return std::make_tuple(divvi, curlvi, dV11i, dV12i, dV13i, dV22i, dV23i, dV33i);
+        else if constexpr (DoCurlV)
+            return std::make_tuple(divvi, curlvi);
+        else if constexpr (DoGradV)
+            return std::make_tuple(divvi, dV11i, dV12i, dV13i, dV22i, dV23i, dV33i);
+        else
+            return std::make_tuple(divvi);
+    }
+};
+
+template<size_t stride = 1, typename Tc, class T>
+HOST_DEVICE_FUN inline void
+divV_curlVJLoop(cstone::LocalIndex i, Tc K, const cstone::Box<Tc>& box, const cstone::LocalIndex* neighbors,
+                unsigned neighborsCount, const Tc* x, const Tc* y, const Tc* z, const T* vx, const T* vy, const T* vz,
+                const T* h, const T* c11, const T* c12, const T* c13, const T* c22, const T* c23, const T* c33,
+                const T* wh, const T* /*whd*/, const T* kx, const T* xm, T* divv, T* curlv, T* dV11, T* dV12, T* dV13,
+                T* dV22, T* dV23, T* dV33, bool doGradV)
+{
+    DivVCurlVInteraction<T> interaction{wh};
+
+    const auto input = std::make_tuple(vx, vy, vz, xm, kx, c11, c12, c13, c22, c23, c33);
+
+    const auto iData  = cstone::ijloop::loadParticleData(x, y, z, h, input, i);
+    const bool usePbc = cstone::ijloop::requiresPbcHandling(box, iData);
+
+    auto result = interaction(iData, iData, cstone::Vec3<Tc>{0, 0, 0}, T(0));
+    for (unsigned pj = 0; pj < neighborsCount; ++pj)
+    {
+        cstone::LocalIndex j = neighbors[stride * pj];
+
+        const auto jData = cstone::ijloop::loadParticleData(x, y, z, h, input, j);
+
+        const auto [r_ij, r2] = cstone::ijloop::posDiffAndDistSq(usePbc, box, iData, jData);
+
+        cstone::ijloop::updateResult(result, interaction(iData, jData, r_ij, r2));
     }
 
-    if (doGradV)
+    if (divv && doGradV)
     {
-        dV11[i] = norm_kxi * dVxi[0];
-        dV12[i] = norm_kxi * (dVxi[1] + dVyi[0]);
-        dV13[i] = norm_kxi * (dVxi[2] + dVzi[0]);
-        dV22[i] = norm_kxi * dVyi[1];
-        dV23[i] = norm_kxi * (dVyi[2] + dVzi[1]);
-        dV33[i] = norm_kxi * dVzi[2];
+        DivVCurlVPostamble<true, true, T, Tc> postamble{K};
+        const auto                            presult = postamble(iData, result);
+        const auto                            output = std::make_tuple(divv, curlv, dV11, dV12, dV13, dV22, dV23, dV33);
+        cstone::ijloop::storeParticleData(output, i, presult);
+    }
+    else if (divv)
+    {
+        DivVCurlVPostamble<true, false, T, Tc> postamble{K};
+        const auto                             presult = postamble(iData, result);
+        const auto                             output  = std::make_tuple(divv, curlv);
+        cstone::ijloop::storeParticleData(output, i, presult);
+    }
+    else if (doGradV)
+    {
+        DivVCurlVPostamble<false, true, T, Tc> postamble{K};
+        const auto                             presult = postamble(iData, result);
+        const auto                             output  = std::make_tuple(divv, dV11, dV12, dV13, dV22, dV23, dV33);
+        cstone::ijloop::storeParticleData(output, i, presult);
+    }
+    else
+    {
+        DivVCurlVPostamble<false, false, T, Tc> postamble{K};
+        const auto                              presult = postamble(iData, result);
+        const auto                              output  = std::make_tuple(divv);
+        cstone::ijloop::storeParticleData(output, i, presult);
     }
 }
 
