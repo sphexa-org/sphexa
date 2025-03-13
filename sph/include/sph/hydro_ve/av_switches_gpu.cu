93b979e78d9ff42b0efee41381f0fac4564b0e30
Felix Thaler <thaler@cscs.ch>
2025-03-13 11:54:17 +0100

    Use neighborhood in computeAVswitches on GPU

--- sph/include/sph/hydro_ve/av_switches_gpu.cu
+++ sph/include/sph/hydro_ve/av_switches_gpu.cu
@@ -32,6 +32,7 @@
 #include "cstone/cuda/cuda_utils.cuh"
 #include "cstone/traversal/find_neighbors.cuh"
 
+#include "sph/neighborhood_gpu.hpp"
 #include "sph/sph_gpu.hpp"
 #include "sph/particles_data.hpp"
 #include "sph/hydro_ve/av_switches_kern.hpp"
@@ -39,62 +40,16 @@
 namespace sph::cuda
 {
 
-using cstone::GpuConfig;
-using cstone::LocalIndex;
-using cstone::TravConfig;
-using cstone::TreeNodeIndex;
-
-template<class Tc, class T, class KeyType>
-__global__ void AVswitchesGpu(Tc K, unsigned ngmax, const cstone::Box<Tc> box, const LocalIndex* grpStart,
-                              const LocalIndex* grpEnd, LocalIndex numGroups,
-                              const cstone::OctreeNsView<Tc, KeyType> tree, const Tc* x, const Tc* y, const Tc* z,
-                              const T* vx, const T* vy, const T* vz, const T* h, const T* c, const T* c11, const T* c12,
-                              const T* c13, const T* c22, const T* c23, const T* c33, const T* wh, const T* whd,
-                              const T* kx, const T* xm, const T* divv, Tc minDt, T alphamin, T alphamax,
-                              T decay_constant, T* alpha, LocalIndex* nidx, TreeNodeIndex* globalPool)
-{
-    unsigned laneIdx     = threadIdx.x & (GpuConfig::warpSize - 1);
-    unsigned targetIdx   = 0;
-    unsigned warpIdxGrid = (blockDim.x * blockIdx.x + threadIdx.x) >> GpuConfig::warpSizeLog2;
-
-    LocalIndex* neighborsWarp = nidx + ngmax * TravConfig::targetSize * warpIdxGrid;
-
-    while (true)
-    {
-        // first thread in warp grabs next target
-        if (laneIdx == 0) { targetIdx = atomicAdd(&cstone::targetCounterGlob, 1); }
-        targetIdx = cstone::shflSync(targetIdx, 0);
-
-        if (targetIdx >= numGroups) return;
-
-        LocalIndex bodyBegin = grpStart[targetIdx];
-        LocalIndex bodyEnd   = grpEnd[targetIdx];
-        LocalIndex i         = bodyBegin + laneIdx;
-
-        auto ncTrue = traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool);
-
-        if (i >= bodyEnd) continue;
-
-        unsigned ncCapped = stl::min(ncTrue[0], ngmax);
-        alpha[i] = AVswitchesJLoop<TravConfig::targetSize>(i, K, box, neighborsWarp + laneIdx, ncCapped, x, y, z, vx,
-                                                           vy, vz, h, c, c11, c12, c13, c22, c23, c33, wh, whd, kx, xm,
-                                                           divv, alpha, minDt, alphamin, alphamax, decay_constant);
-    }
-}
-
 template<class Dataset>
 void computeAVswitches(const GroupView& grp, Dataset& d, const cstone::Box<typename Dataset::RealType>& box)
 {
-    auto [traversalPool, nidxPool] = cstone::allocateNcStacks(d.devData.traversalStack, d.ngmax);
-    cstone::resetTraversalCounters<<<1, 1>>>();
+    // TODO: d.alpha is used as input and output, this fails on neighborhoods that exploit symmetry!
+    AVswitchesIjLoop(getNeighborhoodGpu(d), d.K, d.minDt, d.alphamin, d.alphamax, d.decay_constant,
+                     rawPtr(d.devData.xm), rawPtr(d.devData.kx), rawPtr(d.devData.divv), rawPtr(d.devData.alpha),
+                     rawPtr(d.devData.vx), rawPtr(d.devData.vy), rawPtr(d.devData.vz), rawPtr(d.devData.c),
+                     rawPtr(d.devData.c11), rawPtr(d.devData.c12), rawPtr(d.devData.c13), rawPtr(d.devData.c22),
+                     rawPtr(d.devData.c23), rawPtr(d.devData.c33), rawPtr(d.devData.wh), rawPtr(d.devData.alpha));
 
-    AVswitchesGpu<<<TravConfig::numBlocks(), TravConfig::numThreads>>>(
-        d.K, d.ngmax, box, grp.groupStart, grp.groupEnd, grp.numGroups, d.treeView, rawPtr(d.devData.x),
-        rawPtr(d.devData.y), rawPtr(d.devData.z), rawPtr(d.devData.vx), rawPtr(d.devData.vy), rawPtr(d.devData.vz),
-        rawPtr(d.devData.h), rawPtr(d.devData.c), rawPtr(d.devData.c11), rawPtr(d.devData.c12), rawPtr(d.devData.c13),
-        rawPtr(d.devData.c22), rawPtr(d.devData.c23), rawPtr(d.devData.c33), rawPtr(d.devData.wh),
-        rawPtr(d.devData.whd), rawPtr(d.devData.kx), rawPtr(d.devData.xm), rawPtr(d.devData.divv), d.minDt, d.alphamin,
-        d.alphamax, d.decay_constant, rawPtr(d.devData.alpha), nidxPool, traversalPool);
     checkGpuErrors(cudaDeviceSynchronize());
 }
 
