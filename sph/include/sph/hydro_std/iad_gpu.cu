260611765101609203e73944f61af243aa7fb9e3
Felix Thaler <thaler@cscs.ch>
2025-03-17 07:24:03 +0100

    Move computeIADGpu to neighborhood on GPU

--- sph/include/sph/hydro_std/iad_gpu.cu
+++ sph/include/sph/hydro_std/iad_gpu.cu
@@ -30,95 +30,21 @@
  * @author Sebastian Keller <sebastian.f.keller@gmail.com>
  */
 
-#include "cstone/cuda/cuda_utils.cuh"
-#include "cstone/traversal/find_neighbors.cuh"
-
-#include "sph/sph_gpu.hpp"
+#include "sph/neighborhood_gpu.hpp"
 #include "sph/particles_data.hpp"
 #include "sph/hydro_std/iad_kern.hpp"
 
 namespace sph
 {
 
-using cstone::GpuConfig;
-using cstone::LocalIndex;
-using cstone::TravConfig;
-using cstone::TreeNodeIndex;
-
-/*! @brief
- *
- * @tparam     T               float or double
- * @tparam     KeyType         32- or 64-bit unsigned integer
- * @param[in]  K
- * @param[in]  ngmax           maximum number of neighbors per particle to use
- * @param[in]  box             global coordinate bounding box
- * @param[in]  grpStart        start of each particle group, length @p numGroups
- * @param[in]  grpEnd          end of each particle groups, length @p numGroups
- * @param[in]  numGroups       number of groups
- * @param[in]  numParticles    number of local particles + halos
- * @param[in]  particleKeys    SFC keys of particles, sorted in ascending order
- * @param[in]  x               x coords, length @p numParticles, SFC sorted
- * @param[in]  y               y coords, length @p numParticles, SFC sorted
- * @param[in]  z               z coords, length @p numParticles, SFC sorted
- * @param[in]  h               smoothing lengths, length @p numParticles
- * @param[in]  m               masses, length @p numParticles
- * @param[in]  rho             densities, length @p numParticles
- * @param[in]  wh              sinc lookup table
- * @param[in]  whd             sinc derivative lookup table
- * @param[out] c11             output IAD components, length @p numParticles
- * @param[out] c12
- * @param[out] c13
- * @param[out] c22
- * @param[out] c23
- * @param[out] c33
- */
-template<class Tc, class Tm, class T, class KeyType>
-__global__ void IADGpuKernel(Tc K, unsigned ngmax, cstone::Box<Tc> box, const LocalIndex* grpStart,
-                             const LocalIndex* grpEnd, LocalIndex numGroups,
-                             const cstone::OctreeNsView<Tc, KeyType> tree, const Tc* x, const Tc* y, const Tc* z,
-                             const T* h, const Tm* m, const T* rho, const T* wh, const T* whd, T* c11, T* c12, T* c13,
-                             T* c22, T* c23, T* c33, LocalIndex* nidx, TreeNodeIndex* globalPool)
-{
-    unsigned laneIdx     = threadIdx.x & (GpuConfig::warpSize - 1);
-    unsigned targetIdx   = 0;
-    unsigned warpIdxGrid = (blockDim.x * blockIdx.x + threadIdx.x) >> GpuConfig::warpSizeLog2;
-
-    LocalIndex* neighborsWarp = nidx + ngmax * TravConfig::targetSize * warpIdxGrid;
-
-    while (true)
-    {
-        // first thread in warp grabs next target
-        if (laneIdx == 0) { targetIdx = atomicAdd(&cstone::targetCounterGlob, 1); }
-        targetIdx = cstone::shflSync(targetIdx, 0);
-
-        if (targetIdx >= numGroups) { return; }
-
-        LocalIndex bodyBegin = grpStart[targetIdx];
-        LocalIndex bodyEnd   = grpEnd[targetIdx];
-        LocalIndex i         = bodyBegin + laneIdx;
-
-        auto ncTrue = traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool);
-
-        if (i >= bodyEnd) { continue; }
-
-        unsigned ncCapped = stl::min(ncTrue[0], ngmax);
-        sph::IADJLoopSTD<TravConfig::targetSize>(i, K, box, neighborsWarp + laneIdx, ncCapped, x, y, z, h, m, rho, wh,
-                                                 whd, c11, c12, c13, c22, c23, c33);
-    }
-}
+using cstone::GroupView;
 
 template<class Dataset>
 void computeIADGpu(const GroupView& grp, Dataset& d, const cstone::Box<typename Dataset::RealType>& box)
 {
-    auto [traversalPool, nidxPool] = cstone::allocateNcStacks(d.devData.traversalStack, d.ngmax);
-    cstone::resetTraversalCounters<<<1, 1>>>();
-
-    IADGpuKernel<<<TravConfig::numBlocks(), TravConfig::numThreads>>>(
-        d.K, d.ngmax, box, grp.groupStart, grp.groupEnd, grp.numGroups, d.treeView, rawPtr(d.devData.x),
-        rawPtr(d.devData.y), rawPtr(d.devData.z), rawPtr(d.devData.h), rawPtr(d.devData.m), rawPtr(d.devData.rho),
-        rawPtr(d.devData.wh), rawPtr(d.devData.whd), rawPtr(d.devData.c11), rawPtr(d.devData.c12),
-        rawPtr(d.devData.c13), rawPtr(d.devData.c22), rawPtr(d.devData.c23), rawPtr(d.devData.c33), nidxPool,
-        traversalPool);
+    IADIjLoop(getNeighborhoodGpu(d), d.K, rawPtr(d.devData.m), rawPtr(d.devData.rho), rawPtr(d.devData.wh),
+              rawPtr(d.devData.c11), rawPtr(d.devData.c12), rawPtr(d.devData.c13), rawPtr(d.devData.c22),
+              rawPtr(d.devData.c23), rawPtr(d.devData.c33));
     checkGpuErrors(cudaDeviceSynchronize());
 }
 
