sphexa-org/sphexa

commit d0a4ccd8569ea23bde2ee98b1a6b0d3c78ad185e

Optimized computation of partially symmetric reductions

domain/include/cstone/traversal/ijloop/ijloop.hpp
@@ -198,14 +198,15 @@ struct IsSymmetric<symmetric::odd<T>> : std::true_type
 };
 
 template<class T>
-inline constexpr void applySymmetryImpl(T const&)
+inline constexpr T const& applySymmetryImpl(T const& value)
 {
+    return value;
 }
 
 template<class T>
-inline constexpr void applySymmetryImpl(symmetric::odd<T>& value)
+inline constexpr symmetric::odd<T> applySymmetryImpl(symmetric::odd<T>& value)
 {
-    value.value = -value.value;
+    return {-value.value};
 }
 
 } // namespace detail
@@ -219,10 +220,17 @@ struct IsFullySymmetric<std::tuple<Ts...>> : std::conjunction<detail::IsSymmetri
 };
 
 template<class... Ts>
-inline constexpr void applySymmetry(std::tuple<Ts...>& value)
+inline constexpr auto selectSymmetric(std::tuple<Ts...> const& symmetricValue, std::tuple<Ts...> const& asymmetricValue)
 {
-    if constexpr (IsFullySymmetric<std::tuple<Ts...>>::value)
-        util::for_each_tuple([](auto& v) { detail::applySymmetryImpl(v); }, value);
+    return util::tupleMap(
+        []<class T>(T const& s, T const& a)
+        {
+            if constexpr (detail::IsSymmetric<T>::value)
+                return detail::applySymmetryImpl(s);
+            else
+                return a;
+        },
+        symmetricValue, asymmetricValue);
 }
 
 namespace detail

domain/include/cstone/traversal/ijloop/gpu_clusternblist.cuh
@@ -700,23 +700,19 @@ __global__ __launch_bounds__(GpuConfig::warpSize* NumWarpsPerBlock) void gpuClus
             const auto [ijPosDiff, distSq] = posDiffAndDistSq(UsePbc, box, iData, jData);
             const Th iRadiusSq             = radiusSq(iData);
             const Th jRadiusSq             = radiusSq(jData);
-            auto ijInteraction             = interaction(iData, jData, ijPosDiff, distSq);
+            const auto ijInteraction       = interaction(iData, jData, ijPosDiff, distSq);
             if (distSq < iRadiusSq) updateResult(result, ijInteraction);
             if constexpr (Config::symmetric)
             {
                 if (distSq < jRadiusSq & (!self | (i != j)))
                 {
-                    if constexpr (!IsFullySymmetric<result_t>::value)
-                        ijInteraction = interaction(jData, iData, -ijPosDiff, distSq);
-                    updateResult(jResult, ijInteraction);
+                    const auto jiInteraction =
+                        selectSymmetric(ijInteraction, interaction(jData, iData, -ijPosDiff, distSq));
+                    updateResult(jResult, jiInteraction);
                 }
             }
         }
-        if constexpr (Config::symmetric)
-        {
-            applySymmetry(jResult);
-            storeTupleJSum<Config>(jResult, output, j, j >= firstBody & j < lastBody);
-        }
+        if constexpr (Config::symmetric) storeTupleJSum<Config>(jResult, output, j, j >= firstBody & j < lastBody);
     };
 
     constexpr unsigned jClustersPerWarp     = GpuConfig::warpSize / Config::iSize / Config::jSize;

domain/include/cstone/traversal/ijloop/gpu_superclusternblist.cuh
@@ -944,15 +944,15 @@ __global__ __launch_bounds__(Config::iThreads* Config::jSize* NumSuperclustersPe
                     const auto& iData = iSuperclusterData[c * Config::iSize + block.thread_index().x];
                     assert(std::get<0>(iData) == i - firstValidBody);
                     const auto [ijPosDiff, distSq] = posDiffAndDistSq(UsePbc, box, iData, jData);
-                    auto ijInteraction             = interaction(iData, jData, ijPosDiff, distSq);
+                    const auto ijInteraction       = interaction(iData, jData, ijPosDiff, distSq);
                     if (distSq < radiusSq(iData)) updateResult(iResults[c / iClustersPerWarp], ijInteraction);
                     if constexpr (Config::symmetric)
                     {
                         if ((distSq < radiusSq(jData)) & ((i != j) | (i < firstBody) | (i >= lastBody)))
                         {
-                            if constexpr (!IsFullySymmetric<result_t>::value)
-                                ijInteraction = interaction(jData, iData, -ijPosDiff, distSq);
-                            updateResult(jResult, ijInteraction);
+                            const auto jiInteraction =
+                                selectSymmetric(ijInteraction, interaction(jData, iData, -ijPosDiff, distSq));
+                            updateResult(jResult, jiInteraction);
                         }
                     }
                 }
@@ -962,7 +962,6 @@ __global__ __launch_bounds__(Config::iThreads* Config::jSize* NumSuperclustersPe
 
             if constexpr (Config::symmetric)
             {
-                applySymmetry(jResult);
                 storeTupleJSum<Config>(jResult, output, j, j >= firstBody & j < lastBody);
             }
         }

domain/test/performance/gromacs_ijloop.cuh
@@ -364,18 +364,17 @@ __launch_bounds__(clusterSize* clusterSize) void gromacsLikeNeighborhoodKernel(c
                                 const Tc intBit                = (wexcl & maskJi) ? Tc(1) : Tc(0);
                                 if ((distSq < iRadiusSq) * intBit)
                                 {
-                                    auto ijInteraction = interaction(iData, jData, ijPosDiff, distSq);
+                                    const auto ijInteraction = interaction(iData, jData, ijPosDiff, distSq);
                                     updateResult(iResultBuf[i], ijInteraction);
-                                    if constexpr (!IsFullySymmetric<result_t>())
-                                        ijInteraction = interaction(jData, iData, -ijPosDiff, distSq);
-                                    if (ai != aj) updateResult(jResultBuf, ijInteraction);
+                                    const auto jiInteraction =
+                                        selectSymmetric(ijInteraction, interaction(jData, iData, -ijPosDiff, distSq));
+                                    if (ai != aj) updateResult(jResultBuf, jiInteraction);
                                 }
                             }
                         }
                         maskJi += maskJi;
                     }
 
-                    applySymmetry(jResultBuf);
                     storeTupleJSum(unwrapModifiers(jResultBuf), output, aj, aj >= firstBody & aj < lastBody);
                 }
             }

commit 0bc56c9c97f6049a1e195b75c6c9c682be5098aa

Fix stupid bug with odd symmetry introduced in d0a4ccd8

domain/include/cstone/traversal/ijloop/ijloop.hpp
@@ -204,7 +204,7 @@ inline constexpr T const& applySymmetryImpl(T const& value)
 }
 
 template<class T>
-inline constexpr symmetric::odd<T> applySymmetryImpl(symmetric::odd<T>& value)
+inline constexpr symmetric::odd<T> applySymmetryImpl(symmetric::odd<T> const& value)
 {
     return {-value.value};
 }