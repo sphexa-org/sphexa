1fab0097c3ee41e4471c49618048836876feeaaf
Felix Thaler <thaler@cscs.ch>
2025-03-27 12:09:23 +0200

    Workaround for failing neighborlist compression test

--- domain/include/cstone/compressneighbors.cuh
+++ domain/include/cstone/compressneighbors.cuh
@@ -92,7 +92,12 @@ warpCompressNeighbors(const std::uint32_t* __restrict__ neighbors, char* __restr
         dataSize += nNibblesSize;
 
         if (nonOne) writeDataNibble(nNibblesDataIndex, nNibblesData, false);
+#ifdef __HIP_PLATFORM_AMD__
+        // This should not be necessary, a memory fence should be enough, but tests fail without
+        __syncthreads();
+#else
         syncWarp();
+#endif
         if (nonOne) writeDataNibble(nNibblesDataIndex, nNibblesData, true);
 
         const unsigned nbValueScan      = inclusiveScanInt(nNibbles);
@@ -102,7 +107,12 @@ warpCompressNeighbors(const std::uint32_t* __restrict__ neighbors, char* __restr
 
         for (unsigned i = 0; i < nNibbles; ++i)
             writeDataNibble(nbValueDataIndex + i, (diff >> (4 * i)) & 0xf, false);
+#ifdef __HIP_PLATFORM_AMD__
+        // This should not be necessary, a memory fence should be enough, but tests fail without
+        __syncthreads();
+#else
         syncWarp();
+#endif
         for (unsigned i = 0; i < nNibbles; ++i)
             writeDataNibble(nbValueDataIndex + i, (diff >> (4 * i)) & 0xf, true);
     }
