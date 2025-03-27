e1b3a709f2cd844f482b4bde52eec10f01df7b7f
Felix Thaler <thaler@cscs.ch>
2025-03-27 12:00:38 +0200

    Add missing HIP defines to cuda_runtime.hpp

--- domain/include/cstone/cuda/cuda_runtime.hpp
+++ domain/include/cstone/cuda/cuda_runtime.hpp
@@ -47,9 +47,11 @@
 #define cudaMemcpyFromSymbol hipMemcpyFromSymbol
 #define cudaMemcpyHostToDevice hipMemcpyHostToDevice
 #define cudaMemcpyToSymbol hipMemcpyToSymbol
+#define cudaMemcpyToSymbolAsync hipMemcpyToSymbol // hipMemcpyToSymbolAsync does not exist
 #define cudaMemoryTypeDevice hipMemoryTypeDevice
 #define cudaMemoryTypeManaged hipMemoryTypeManaged
 #define cudaMemset hipMemset
+#define cudaMemsetAsync hipMemsetAsync
 #define cudaPointerAttributes hipPointerAttribute_t
 #define cudaPointerGetAttributes hipPointerGetAttributes
 #define cudaSetDevice hipSetDevice
@@ -60,6 +62,8 @@
 #define cudaSuccess hipSuccess
 
 #define GPU_SYMBOL HIP_SYMBOL
+
+#define __grid_constant__
 
 #else
 
