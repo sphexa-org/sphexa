6b7fa73e44618af7ff67c0c8fcfca547711eda7b
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-04-04 15:01:12 +0200

    Implementation of InitSetting type and setting writing function

--- main/src/init/settings.hpp
+++ main/src/init/settings.hpp
@@ -27,12 +27,17 @@
  * @brief Translation unit for the simulation initializer library
  *
  * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ * @author Christopher Bignamini <christopher.bignamini@gmail.com>
  */
 
 #pragma once
 
 #include <filesystem>
+#include <map>
 #include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
 
 #include "io/ifile_io.hpp"
 #include "io/id_tag_utils.hpp"
@@ -40,29 +45,59 @@
 namespace sphexa
 {
 
-using InitSettings = std::map<std::string, double>;
+using ScalarValue = double;
+using VectorValue = std::vector<IdType>;
 
-// TODO: alternatively, I could generalize InitSettings
+//! @brief Wrapper for scalar-type and vector-type parameters
+struct Param {
+
+    std::variant<ScalarValue, VectorValue> value;
+
+public:
+
+    Param(void) : value(ScalarValue{}) {}
+
+    // Conversion of all arithmetic types to ScalarValue
+    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T> || std::is_enum_v<T>>>
+    Param(T v) : value(static_cast<ScalarValue>(v)) {}
+
+    Param(const VectorValue& v) : value(v) {}
+    Param(VectorValue&& v) : value(std::move(v)) {}
+
+    // Conversion of all arithmetic types to ScalarValue
+    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T> || std::is_enum_v<T>>>
+    Param& operator=(T v) { value = static_cast<ScalarValue>(v); return *this; }
+
+    Param& operator=(const VectorValue& v) { value = v; return *this; }
+    Param& operator=(VectorValue&& v) { value = std::move(v); return *this; }
+
+    const auto& getValue() const { return value; }
+    auto& getValue() { return value; }
+
+    // Check stored type
+    bool isScalar() const { return std::holds_alternative<ScalarValue>(value); }
+    bool isVector() const { return std::holds_alternative<VectorValue>(value); }
+
+    // Implicit conversion to std::variant
+    operator std::variant<ScalarValue, VectorValue>() const { return value; }
+
+};
+
+using InitSettings = std::map<std::string, Param>;
+
 struct IdSelectionSettings
 {
-    // TODO: the second variant case is not really useful
     // TODO: does it make sense to have an id-range based selection?
-//    using IdSelectionTypeVariant = std::variant<IdSelectionSphere, std::vector<IdType>>;
-    using IdSelectionTypeVariant = IdSelectionSphere;
+    using IdSelectionType = std::variant<IdSelectionList, IdSelectionSphere>;
 
     // TODO: this data is already stored in settings_,
-    IdSelectionTypeVariant selectionData;
+    // can we store here the just keys to retrieve the stored data?
+    IdSelectionType selectionData;
 
-    unsigned int selectionTimeStep;
+    int selectionTimeStep;
 };
 
-struct IdSelectionDataset
-{
-    IdSelectionSettings settings;
-    std::vector<IdType> ids;
-};
-
-using IdSubsets = std::map<std::string, IdSelectionDataset>;
+using IdSubsets= std::map<std::string, IdSelectionSettings>;
 
 //! @brief write @p InitSettings as file attributes of a new file @p path
 inline void writeSettings(const InitSettings& settings, const std::string& path, IFileWriter* writer)
@@ -75,7 +110,13 @@ inline void writeSettings(const InitSettings& settings, const std::string& path,
     writer->addStep(0, 0, path, true);
     for (auto it = settings.cbegin(); it != settings.cend(); ++it)
     {
-        writer->fileAttribute(it->first, &(it->second), 1);
+        if(std::holds_alternative<ScalarValue>(it->second.value)) {
+            writer->fileAttribute(it->first, &(std::get<ScalarValue>(it->second.value)), 1);
+        }
+        else {
+            // TODO: if the id selection list is removed after init, this line is never called. Otherwise, we need to define what we want
+            writer->fileAttribute(it->first, std::get<VectorValue>(it->second.value).data(), std::get<VectorValue>(it->second.value).size());
+        }
     }
     writer->closeStep();
 }
@@ -91,13 +132,21 @@ inline void writeSettings(const IdSubsets& idSubsets, const std::string& path, I
     writer->addStep(0, 0, path, true);
     for (auto it = idSubsets.cbegin(); it != idSubsets.cend(); ++it)
     {
-        writer->fileAttribute(it->first + "_radius", &(it->second.settings.selectionData.radius), 1);
-        writer->fileAttribute(it->first + "_center_x", &(it->second.settings.selectionData.center[0]), 1);
-        writer->fileAttribute(it->first + "_center_y", &(it->second.settings.selectionData.center[1]), 1);
-        writer->fileAttribute(it->first + "_center_z", &(it->second.settings.selectionData.center[2]), 1);
-        writer->fileAttribute(it->first + "_time_step", &(it->second.settings.selectionTimeStep), 1);
-        // TODO: I would not write the ids as attrbutes but as fields
-        writer->fileAttribute(it->first + "_id_subset", it->second.ids.data(), (it->second.ids.size()<10 ? it->second.ids.size() : 10));
+        if(std::holds_alternative<IdSelectionSphere>(it->second.selectionData)) {
+            const IdSelectionSphere& idSelectionSphere(std::get<IdSelectionSphere>(it->second.selectionData));
+            writer->fileAttribute(it->first + "_radius", &(idSelectionSphere.radius), 1);
+            writer->fileAttribute(it->first + "_center_x", &(idSelectionSphere.center[0]), 1);
+            writer->fileAttribute(it->first + "_center_y", &(idSelectionSphere.center[1]), 1);
+            writer->fileAttribute(it->first + "_center_z", &(idSelectionSphere.center[2]), 1);
+        }
+        else if(std::holds_alternative<IdSelectionList>(it->second.selectionData)) {
+            const IdSelectionList& idSelectionList(std::get<IdSelectionList>(it->second.selectionData));
+            writer->fileAttribute(it->first + "_id_subset", idSelectionList.data(), idSelectionList.size());
+        }
+        else {
+            throw std::runtime_error("Cannot write settings: unsupported id subset type\n");
+        }
+        writer->fileAttribute(it->first + "_time_step", &(it->second.selectionTimeStep), 1);
     }
     writer->closeStep();
 }
@@ -117,7 +166,16 @@ public:
 
     void stepAttribute(const std::string& key, FieldType val, int64_t /*size*/)
     {
-        std::visit([this, &key](auto arg) { *arg = attributes_.at(key); }, val);
+        std::visit([this, &key](auto arg) {
+            if(std::holds_alternative<ScalarValue>(attributes_.at(key).getValue())) {
+                *arg = std::get<ScalarValue>(attributes_.at(key).getValue());
+            }
+            else {
+                // TODO: I don't have implemented this case because it seems
+                // not to be used in the current version of the code
+                std::runtime_error("BuiltinWriter: unsupported type");
+            }
+        }, val);
     };
 
 private:
