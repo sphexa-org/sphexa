2493e14cd18f1673782ee6e0e822fc89109e3fe6
Felix Thaler <thaler@cscs.ch>
2025-04-07 13:36:08 +0300

    Fix rsqrt usage on host

--- domain/test/performance/lennard_jones_driver.cu
+++ domain/test/performance/lennard_jones_driver.cu
@@ -59,7 +59,11 @@ struct LjKernelFun
         const auto [i, iPos, hi, qi] = iData;
         const auto [j, jPos, hj, qj] = jData;
         const T r2                   = std::max(distSq, T(1e-1));
+#if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__)
         const T rinv                 = rsqrt(r2);
+#else
+        const T rinv                 = T(1) / std::sqrt(r2);
+#endif
         const T r2inv                = rinv * rinv;
         const T r6inv                = r2inv * r2inv * r2inv;
         const T forcelj              = r6inv * (lj1 * r6inv - lj2) * r2inv;
