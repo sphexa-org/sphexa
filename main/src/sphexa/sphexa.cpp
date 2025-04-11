15728328e63091c5e564d5eddb173e76ff8f43e8
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-04-11 10:58:49 +0200

    Comment removed

--- main/src/sphexa/sphexa.cpp
+++ main/src/sphexa/sphexa.cpp
@@ -189,7 +189,6 @@ int main(int argc, char** argv)
 
         if (isSubsetOutputTriggered)
         {
-            // TODO: check if we need addStep, saveFields, save, closeStep
             propagator->saveSubsetFields(fileWriter.get(), outFileSubset, domain.startIndex(), domain.endIndex(), simData.hydro);
             isSubsetOutputTriggered = false;
         }
