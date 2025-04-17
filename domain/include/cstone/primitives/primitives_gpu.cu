00fb2c997e40c43f688bcf678acff2622bda300c
ChristoperBignamini <christopher.bignamini@gmail.com>
2025-04-17 18:31:17 +0200

    gatherGpu template specializations added

--- domain/include/cstone/primitives/primitives_gpu.cu
+++ domain/include/cstone/primitives/primitives_gpu.cu
@@ -131,6 +131,12 @@ template void gatherGpu(const unsigned*, size_t, const util::array<float, 2>*, u
 template void gatherGpu(const unsigned*, size_t, const util::array<float, 3>*, util::array<float, 3>*);
 template void gatherGpu(const unsigned*, size_t, const util::array<float, 4>*, util::array<float, 4>*);
 
+template void gatherGpu(const unsigned long*, size_t, const uint8_t*, uint8_t*);
+template void gatherGpu(const unsigned long*, size_t, const float*, float*);
+template void gatherGpu(const unsigned long*, size_t, const double*, double*);
+template void gatherGpu(const unsigned long*, size_t, const unsigned int*, unsigned int*);
+template void gatherGpu(const unsigned long*, size_t, const unsigned long*, unsigned long*);
+
 template<class T, class IndexType>
 __global__ void scatterGpuKernel(const IndexType* map, size_t n, const T* source, T* destination)
 {
