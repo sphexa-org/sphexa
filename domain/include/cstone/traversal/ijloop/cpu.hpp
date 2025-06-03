af885f17609ea3bce2dcac3a821ddab060bdfc2e
Felix Thaler <thaler@cscs.ch>
2025-06-03 10:54:56 +0200

    Factor out jLoop in CPU ijloop implementations

--- domain/include/cstone/traversal/ijloop/cpu.hpp
+++ domain/include/cstone/traversal/ijloop/cpu.hpp
@@ -68,28 +68,39 @@ struct CpuAlwaysTraverseNeighborhoodImpl
 
 #pragma omp for
             for (LocalIndex i = firstBody; i < lastBody; ++i)
-            {
-                const auto iData  = loadParticleData(x, y, z, h, constInput, i);
-                const bool usePbc = requiresPbcHandling(box, iData);
-
-                const unsigned nbs = std::min(findNeighbors(i, x, y, z, h, tree, box, ngmax, neighbors.get()), ngmax);
-                auto result        = interaction(iData, iData, Vec3<Tc>{0, 0, 0}, Tc(0));
-                for (unsigned nb = 0; nb < nbs; ++nb)
-                {
-                    const LocalIndex j = neighbors[nb];
-                    const auto jData   = loadParticleData(x, y, z, h, constInput, j);
-
-                    const auto [ijPosDiff, distSq] = posDiffAndDistSq(usePbc, box, iData, jData);
-
-                    updateResult(result, interaction(iData, jData, ijPosDiff, distSq));
-                }
-
-                storeParticleData(output, i, postamble(iData, unwrapModifiers(result)));
-            }
+                jLoop(constInput, output, std::forward<Interaction>(interaction), std::forward<Postamble>(postamble), i,
+                      neighbors.get());
         }
     }
 
     Statistics stats() const { return {.numBodies = lastBody - firstBody, .numBytes = 0}; }
+
+protected:
+    template<class Input, class Output, class Interaction, class Postamble>
+    void jLoop(Input&& input,
+               Output&& output,
+               Interaction&& interaction,
+               Postamble&& postamble,
+               const LocalIndex i,
+               LocalIndex* neighbors) const
+    {
+        const auto iData  = loadParticleData(x, y, z, h, std::forward<Input>(input), i);
+        const bool usePbc = requiresPbcHandling(box, iData);
+
+        const unsigned nbs = std::min(findNeighbors(i, x, y, z, h, tree, box, ngmax, neighbors), ngmax);
+        auto result        = interaction(iData, iData, Vec3<Tc>{0, 0, 0}, Tc(0));
+        for (unsigned nb = 0; nb < nbs; ++nb)
+        {
+            const LocalIndex j = neighbors[nb];
+            const auto jData   = loadParticleData(x, y, z, h, std::forward<Input>(input), j);
+
+            const auto [ijPosDiff, distSq] = posDiffAndDistSq(usePbc, box, iData, jData);
+
+            updateResult(result, interaction(iData, jData, ijPosDiff, distSq));
+        }
+
+        storeParticleData(std::forward<Output>(output), i, postamble(iData, unwrapModifiers(result)));
+    }
 };
 
 template<class Tc, class KeyType, class Th>
@@ -112,24 +123,7 @@ struct CpuFullNbListNeighborhoodImpl
         const auto constInput = makeConstRestrict(input);
 #pragma omp parallel for simd
         for (LocalIndex i = firstBody; i < lastBody; ++i)
-        {
-            const auto iData  = loadParticleData(x, y, z, h, constInput, i);
-            const bool usePbc = requiresPbcHandling(box, iData);
-
-            const unsigned nbs = neighborsCount[i - firstBody];
-            auto result        = interaction(iData, iData, Vec3<Tc>{0, 0, 0}, Tc(0));
-            for (unsigned nb = 0; nb < nbs; ++nb)
-            {
-                const LocalIndex j = neighbors[(i - firstBody) * ngmax + nb];
-                const auto jData   = loadParticleData(x, y, z, h, constInput, j);
-
-                const auto [ijPosDiff, distSq] = posDiffAndDistSq(usePbc, box, iData, jData);
-
-                if (distSq < radiusSq(iData)) updateResult(result, interaction(iData, jData, ijPosDiff, distSq));
-            }
-
-            storeParticleData(output, i, postamble(iData, unwrapModifiers(result)));
-        }
+            jLoop(constInput, output, std::forward<Interaction>(interaction), std::forward<Postamble>(postamble), i);
     }
 
     Statistics stats() const
@@ -137,6 +131,29 @@ struct CpuFullNbListNeighborhoodImpl
         const LocalIndex numBodies = lastBody - firstBody;
         return {.numBodies = numBodies,
                 .numBytes  = sizeof(LocalIndex) * numBodies + sizeof(LocalIndex) * numBodies * ngmax};
+    }
+
+protected:
+    template<class Input, class Output, class Interaction, class Postamble>
+    void
+    jLoop(Input&& input, Output&& output, Interaction&& interaction, Postamble&& postamble, const LocalIndex i) const
+    {
+        const auto iData  = loadParticleData(x, y, z, h, std::forward<Input>(input), i);
+        const bool usePbc = requiresPbcHandling(box, iData);
+
+        const unsigned nbs = neighborsCount[i - firstBody];
+        auto result        = interaction(iData, iData, Vec3<Tc>{0, 0, 0}, Tc(0));
+        for (unsigned nb = 0; nb < nbs; ++nb)
+        {
+            const LocalIndex j = neighbors[(i - firstBody) * ngmax + nb];
+            const auto jData   = loadParticleData(x, y, z, h, std::forward<Input>(input), j);
+
+            const auto [ijPosDiff, distSq] = posDiffAndDistSq(usePbc, box, iData, jData);
+
+            if (distSq < radiusSq(iData)) updateResult(result, interaction(iData, jData, ijPosDiff, distSq));
+        }
+
+        storeParticleData(std::forward<Output>(output), i, postamble(iData, unwrapModifiers(result)));
     }
 };
 } // namespace cpu_nb_list_detail
