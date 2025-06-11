db1ea7aecc56a1dbd6b144a244e6612a53495bf8
Felix Thaler <thaler@cscs.ch>
2025-06-11 08:30:54 +0200

    Added documentation to the cstone::ijloop::symmetric and cstone::ijloop::reduction wrappers

--- domain/include/cstone/traversal/ijloop/ijloop.hpp
+++ domain/include/cstone/traversal/ijloop/ijloop.hpp
@@ -46,12 +46,22 @@ namespace cstone::ijloop
 namespace symmetric
 {
 
+/*! Wrapper struct to mark a value as being evenly symmetric.
+ *
+ * This can be used to indicate that the contained value returned from a particle-particle interaction exhibits even
+ * symmetry, i.e., f(i, j) == f(j, i).
+ */
 template<class T>
 struct even
 {
     T value = {};
 };
 
+/*! Wrapper struct to mark a value as being oddly symmetric.
+ *
+ * This can be used to indicate that the contained value returned from a particle-particle interaction exhibits odd
+ * symmetry, i.e., f(i, j) == -f(j, i).
+ */
 template<class T>
 struct odd
 {
@@ -63,12 +73,22 @@ struct odd
 namespace reduction
 {
 
+/*! Wrapper struct to mark a value as requiring a minimum reduction instead of the default sum.
+ *
+ * This can be used to indicate that in the neighbor reduction loop, the minimum of all neighbor values should be used
+ * instead of the sum, i.e., f(i) = min(f(i, j) for all neighbors j).
+ */
 template<class T>
 struct min
 {
     T value = std::numeric_limits<T>::max();
 };
 
+/*! Wrapper struct to mark a value as requiring a maximum reduction instead of the default sum.
+ *
+ * This can be used to indicate that in the neighbor reduction loop, the maximum of all neighbor values should be used
+ * instead of the sum, i.e., f(i) = max(f(i, j) for all neighbors j).
+ */
 template<class T>
 struct max
 {
