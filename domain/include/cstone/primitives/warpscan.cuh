1c449f735ef6f2ba1873960cb908fed20fd3c599
Felix Thaler <thaler@cscs.ch>
2025-06-23 14:39:53 +0200

    Use laneIdx where applicable

--- domain/include/cstone/primitives/warpscan.cuh
+++ domain/include/cstone/primitives/warpscan.cuh
@@ -144,7 +144,7 @@ __device__ __forceinline__ T warpMax(T laneVal)
 //! @brief standard inclusive warp-scan
 __device__ __forceinline__ int inclusiveScanInt(int value)
 {
-    unsigned lane = threadIdx.x & (GpuConfig::warpSize - 1);
+    unsigned lane = laneIndex();
 #pragma unroll
     for (int i = 1; i < GpuConfig::warpSize; i *= 2)
     {
@@ -157,7 +157,7 @@ __device__ __forceinline__ int inclusiveScanInt(int value)
 //! @brief returns a mask with bits set for each warp lane before the calling lane
 __device__ __forceinline__ GpuConfig::ThreadMask lanemask_lt()
 {
-    GpuConfig::ThreadMask lane = threadIdx.x & (GpuConfig::warpSize - 1);
+    GpuConfig::ThreadMask lane = laneIndex();
     return (GpuConfig::ThreadMask(1) << lane) - 1;
 }
 
@@ -180,7 +180,7 @@ __device__ __forceinline__ int reduceBool(const bool p)
 //! @brief returns a mask with bits set for each warp lane before and including the calling lane
 __device__ __forceinline__ GpuConfig::ThreadMask lanemask_le()
 {
-    GpuConfig::ThreadMask lane = threadIdx.x & (GpuConfig::warpSize - 1);
+    GpuConfig::ThreadMask lane = laneIndex();
     return (GpuConfig::ThreadMask(2) << lane) - 1;
 }
 
@@ -218,7 +218,7 @@ __device__ __forceinline__ int inclusiveSegscan(int value, int distance)
  */
 __device__ __forceinline__ int inclusiveSegscanInt(const int packedValue, const int carryValue)
 {
-    int laneIdx = int(threadIdx.x) & (GpuConfig::warpSize - 1);
+    int laneIdx = laneIndex();
 
     int isNegative = packedValue < 0;
     int mask       = -isNegative;
@@ -269,7 +269,7 @@ __device__ __forceinline__ int streamCompact(T* value, bool keep, volatile T* sm
     if (keep) { sm_exchange[laneCompacted] = *value; }
     syncWarp();
 
-    int laneIdx = threadIdx.x & (GpuConfig::warpSize - 1);
+    int laneIdx = laneIndex();
     *value      = sm_exchange[laneIdx];
 
     int numKeep = popCount(SignedMask(keepBallot));
@@ -284,7 +284,7 @@ __device__ __forceinline__ int streamCompact(T* value, bool keep, volatile T* sm
  */
 __device__ __forceinline__ int spreadSeg8(int val)
 {
-    int laneIdx = int(threadIdx.x) & (GpuConfig::warpSize - 1);
+    int laneIdx = laneIndex();
     return shflSync(val, laneIdx >> 3) + (laneIdx & 7);
 }
 
