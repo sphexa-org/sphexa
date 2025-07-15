5353374318921c97c0f0c9c970f0227e1b27827e
Sebastian Keller <44800242+sekelle@users.noreply.github.com>
2025-07-15 14:14:07 +0200

    fix x-y-z size assert (#514)

--- main/src/init/grid.hpp
+++ main/src/init/grid.hpp
@@ -254,7 +254,7 @@ void assembleCuboid(KeyType keyStart, KeyType keyEnd, const cstone::Box<T>& glob
                     std::span<const T> xBlock, std::span<const T> yBlock, std::span<const T> zBlock, Vector& x,
                     Vector& y, Vector& z)
 {
-    assert(x.size() == y.size() == z.size());
+    assert(x.size() == y.size() && x.size() == z.size());
     auto outOfBounds = [](auto it1, auto it2)
     { return *std::min_element(it1, it2) < 0.0 || *std::max_element(it1, it2) >= 1.0; };
     if (outOfBounds(xBlock.begin(), xBlock.end()) || outOfBounds(yBlock.begin(), yBlock.end()) ||
