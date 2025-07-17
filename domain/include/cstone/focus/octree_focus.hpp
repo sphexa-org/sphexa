unibas-dmi-hpc/SPH-EXA

renamed findPeers to focusPeers

domain/include/cstone/focus/octree_focus.hpp
@@ -289,11 +289,11 @@ private:
 
 //! @brief Return a list of ranks (peers) which contain nodes in @p focusTree that don't exist in @p globalTree
 template<class KeyType>
-std::vector<int> oneSidedPeerFlags(gsl::span<const KeyType> boundaries,
-                                   int numRanks,
-                                   int myRank,
-                                   gsl::span<const KeyType> globalTree,
-                                   gsl::span<const KeyType> focusTree)
+std::vector<int> focusPeers(gsl::span<const KeyType> boundaries,
+                            int numRanks,
+                            int myRank,
+                            gsl::span<const KeyType> globalTree,
+                            gsl::span<const KeyType> focusTree)
 {
     std::vector<int> peerFlags(numRanks, 0);
 #pragma omp parallel for
@@ -320,13 +320,32 @@ std::vector<int> oneSidedPeers(gsl::span<const KeyType> boundaries,
                                gsl::span<const KeyType> globalTree,
                                gsl::span<const KeyType> focusTree)
 {
-    auto peerFlags = oneSidedPeerFlags(boundaries, numRanks, myRank, globalTree, focusTree);
+    auto peerFlags = focusPeers(boundaries, numRanks, myRank, globalTree, focusTree);
     std::vector<int> ret;
     for (int rank = 0; rank < numRanks; ++rank)
     {
         if (peerFlags[rank]) { ret.push_back(rank); }
     }
     return ret;
+}
+
+inline std::vector<int>
+haloPeers(int myRank, gsl::span<const int> haloFlags, gsl::span<const TreeIndexPair> fAssignment)
+{
+    int numRanks = fAssignment.size();
+    std::vector<int> peerFlags(numRanks, 0);
+#pragma omp parallel for
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (rank == myRank) { continue; }
+
+        TreeNodeIndex focStart = fAssignment[rank].start();
+        TreeNodeIndex focEnd   = fAssignment[rank].end();
+        if (focEnd < focStart) { focEnd = focStart; }
+
+        peerFlags[rank] = bool(std::accumulate(haloFlags.begin() + focStart, haloFlags.begin() + focEnd, 0));
+    }
+    return peerFlags;
 }
 
 } // namespace cstone

domain/include/cstone/focus/octree_focus_mpi.hpp
@@ -564,8 +564,8 @@ public:
 private:
     void findPeers(const SfcAssignment<KeyType>& assignment, gsl::span<const KeyType> globalLeaves)
     {
-        auto recvPeers = oneSidedPeerFlags<KeyType>({assignment.data(), size_t(numRanks_ + 1)}, numRanks_, myRank_,
-                                                    globalLeaves, leaves_);
+        auto recvPeers =
+            focusPeers<KeyType>({assignment.data(), size_t(numRanks_ + 1)}, numRanks_, myRank_, globalLeaves, leaves_);
         std::vector<int> sendPeers(numRanks_, 0);
         MPI_Alltoall(recvPeers.data(), 1, MPI_INT, sendPeers.data(), 1, MPI_INT, MPI_COMM_WORLD);
 

sphexa-org/sphexa

domain/include/cstone/focus/octree_focus.hpp
@@ -403,7 +403,7 @@ private:
 
 //! @brief Return a list of ranks (peers) which contain nodes in @p focusTree that don't exist in @p globalTree
 template<class KeyType>
-std::vector<int> oneSidedPeerFlags(std::span<const KeyType> boundaries,
+std::vector<int> focusPeers(std::span<const KeyType> boundaries,
                                    int numRanks,
                                    int myRank,
                                    std::span<const KeyType> globalTree,
@@ -434,13 +434,32 @@ std::vector<int> oneSidedPeers(std::span<const KeyType> boundaries,
                                std::span<const KeyType> globalTree,
                                std::span<const KeyType> focusTree)
 {
-    auto peerFlags = oneSidedPeerFlags(boundaries, numRanks, myRank, globalTree, focusTree);
+    auto peerFlags = focusPeers(boundaries, numRanks, myRank, globalTree, focusTree);
     std::vector<int> ret;
     for (int rank = 0; rank < numRanks; ++rank)
     {
         if (peerFlags[rank]) { ret.push_back(rank); }
     }
     return ret;
+}
+
+inline std::vector<int>
+haloPeers(int myRank, std::span<const int> haloFlags, std::span<const TreeIndexPair> fAssignment)
+{
+    int numRanks = fAssignment.size();
+    std::vector<int> peerFlags(numRanks, 0);
+#pragma omp parallel for
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (rank == myRank) { continue; }
+
+        TreeNodeIndex focStart = fAssignment[rank].start();
+        TreeNodeIndex focEnd   = fAssignment[rank].end();
+        if (focEnd < focStart) { focEnd = focStart; }
+
+        peerFlags[rank] = bool(std::accumulate(haloFlags.begin() + focStart, haloFlags.begin() + focEnd, 0));
+    }
+    return peerFlags;
 }
 
 } // namespace cstone

domain/include/cstone/focus/octree_focus_mpi.hpp
@@ -683,9 +683,9 @@ private:
             memcpyD2H(globalLeaves.data(), globalLeaves.size(), globalTreeBackingBuffer.data());
             globalLeaves = std::span(globalTreeBackingBuffer);
         }
-        auto extPeers = oneSidedPeerFlags<KeyType>({assignment.data(), size_t(numRanks_ + 1)}, numRanks_, myRank_,
-                                                   globalLeaves, leaves_);
 
+        auto extPeers =
+            focusPeers<KeyType>({assignment.data(), size_t(numRanks_ + 1)}, numRanks_, myRank_, globalLeaves, leaves_);
         std::vector<int> intPeers(numRanks_, 0);
         MPI_Alltoall(extPeers.data(), 1, MPI_INT, intPeers.data(), 1, MPI_INT, MPI_COMM_WORLD);
 