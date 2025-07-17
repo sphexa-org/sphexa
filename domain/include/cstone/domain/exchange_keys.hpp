unibas-dmi-hpc/SPH-EXA

added onesided findPeers in FocusedOctree

domain/include/cstone/domain/exchange_keys.hpp
@@ -63,17 +63,18 @@ template<class KeyType>
 SendList exchangeRequestKeys(gsl::span<const KeyType> treeLeaves,
                              gsl::span<const int> haloFlags,
                              gsl::span<const TreeIndexPair> assignment,
-                             gsl::span<const int> peerRanks,
+                             gsl::span<const int> sendRanks,
+                             gsl::span<const int> recvRanks,
                              gsl::span<const LocalIndex> layout)
 {
     std::vector<std::vector<KeyType>> sendBuffers;
-    sendBuffers.reserve(peerRanks.size());
+    sendBuffers.reserve(sendRanks.size());
 
     std::vector<MPI_Request> sendRequests;
 
     constexpr int haloRequestKeyTag = static_cast<int>(P2pTags::haloRequestKeys);
 
-    for (int peer : peerRanks)
+    for (int peer : sendRanks)
     {
         auto requestKeys =
             extractMarkedElements(treeLeaves, haloFlags, assignment[peer].start(), assignment[peer].end());
@@ -85,8 +86,8 @@ SendList exchangeRequestKeys(gsl::span<const KeyType> treeLeaves,
 
     SendList ret(assignment.size());
 
-    size_t numMessages = peerRanks.size();
-    while (numMessages > 0)
+    int numMessages = recvRanks.ssize();
+    while (numMessages--)
     {
         MPI_Status status;
         mpiRecvSync(receiveBuffer.data(), receiveBuffer.size(), MPI_ANY_SOURCE, haloRequestKeyTag, &status);
@@ -104,8 +105,6 @@ SendList exchangeRequestKeys(gsl::span<const KeyType> treeLeaves,
 
             ret[receiveRank].addRange(lowerIdx, upperIdx);
         }
-
-        numMessages--;
     }
 
     MPI_Status status[sendRequests.size()];

domain/include/cstone/focus/octree_focus.hpp
@@ -287,4 +287,46 @@ private:
     std::vector<char> macs_;
 };
 
+//! @brief Return a list of ranks (peers) which contain nodes in @p focusTree that don't exist in @p globalTree
+template<class KeyType>
+std::vector<int> oneSidedPeerFlags(gsl::span<const KeyType> boundaries,
+                                   int numRanks,
+                                   int myRank,
+                                   gsl::span<const KeyType> globalTree,
+                                   gsl::span<const KeyType> focusTree)
+{
+    std::vector<int> peerFlags(numRanks, 0);
+#pragma omp parallel for
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (rank == myRank) { continue; }
+        auto globStart = std::lower_bound(globalTree.begin(), globalTree.end(), boundaries[rank]);
+        auto globEnd   = std::lower_bound(globalTree.begin(), globalTree.end(), boundaries[rank + 1]);
+
+        auto focStart = std::lower_bound(focusTree.begin(), focusTree.end(), boundaries[rank]);
+        auto focEnd   = std::upper_bound(focusTree.begin(), focusTree.end(), boundaries[rank + 1]) - 1;
+        if (focEnd < focStart) { focEnd = focStart; }
+
+        if (focEnd - focStart > globEnd - globStart) { peerFlags[rank] = 1; }
+        else { peerFlags[rank] = not std::includes(globStart, globEnd, focStart, focEnd); }
+    }
+    return peerFlags;
+}
+
+template<class KeyType>
+std::vector<int> oneSidedPeers(gsl::span<const KeyType> boundaries,
+                               int numRanks,
+                               int myRank,
+                               gsl::span<const KeyType> globalTree,
+                               gsl::span<const KeyType> focusTree)
+{
+    auto peerFlags = oneSidedPeerFlags(boundaries, numRanks, myRank, globalTree, focusTree);
+    std::vector<int> ret;
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (peerFlags[rank]) { ret.push_back(rank); }
+    }
+    return ret;
+}
+
 } // namespace cstone

sphexa-org/sphexa

domain/include/cstone/domain/exchange_keys.hpp
@@ -27,7 +27,8 @@ namespace cstone
  * @param treeLeaves    cornerstone octree leaves, length N+1
  * @param assignment    assignment of @p treeLeaves to ranks, only ranks listed in @p peerRanks
  *                      are accessed
- * @param peerRanks     list of peer rank IDs
+ * @param sendRanks     list of exterior peer rank IDs
+ * @param recvRanks     list of interior peer rank IDs
  * @param layout        particle location (index in buffers) of each node in @a treeLeaves
  * @return              a SendList, containing ranges of local particle indices to send out
  *                      to each peer rank in subsequent halo particle exchanges.
@@ -44,17 +45,18 @@ namespace cstone
 template<class KeyType>
 SendList exchangeRequestKeys(std::span<const KeyType> treeLeaves,
                              std::span<const TreeIndexPair> assignment,
-                             std::span<const int> peerRanks,
+                             std::span<const int> sendRanks,
+                             std::span<const int> recvRanks,
                              std::span<const LocalIndex> layout)
 {
     std::vector<std::vector<KeyType>> sendBuffers;
-    sendBuffers.reserve(peerRanks.size());
+    sendBuffers.reserve(sendRanks.size());
 
     std::vector<MPI_Request> sendRequests;
 
     constexpr int haloRequestKeyTag = static_cast<int>(P2pTags::haloRequestKeys);
 
-    for (int peer : peerRanks)
+    for (int peer : sendRanks)
     {
         auto requestKeys = extractMarkedElements(treeLeaves, layout, assignment[peer].start(), assignment[peer].end());
         mpiSendAsync(requestKeys.data(), int(requestKeys.size()), peer, haloRequestKeyTag, sendRequests);
@@ -65,8 +67,8 @@ SendList exchangeRequestKeys(std::span<const KeyType> treeLeaves,
 
     SendList ret(assignment.size());
 
-    size_t numMessages = peerRanks.size();
-    while (numMessages > 0)
+    int numMessages = int(recvRanks.size());
+    while (numMessages--)
     {
         MPI_Status status;
         mpiRecvSync(receiveBuffer.data(), receiveBuffer.size(), MPI_ANY_SOURCE, haloRequestKeyTag, &status);
@@ -84,8 +86,6 @@ SendList exchangeRequestKeys(std::span<const KeyType> treeLeaves,
 
             ret[receiveRank].addRange(lowerIdx, upperIdx);
         }
-
-        numMessages--;
     }
 
     MPI_Status status[sendRequests.size()];

domain/include/cstone/focus/octree_focus.hpp
@@ -401,4 +401,46 @@ private:
     std::vector<uint8_t> macs_;
 };
 
+//! @brief Return a list of ranks (peers) which contain nodes in @p focusTree that don't exist in @p globalTree
+template<class KeyType>
+std::vector<int> oneSidedPeerFlags(std::span<const KeyType> boundaries,
+                                   int numRanks,
+                                   int myRank,
+                                   std::span<const KeyType> globalTree,
+                                   std::span<const KeyType> focusTree)
+{
+    std::vector<int> peerFlags(numRanks, 0);
+#pragma omp parallel for
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (rank == myRank) { continue; }
+        auto globStart = std::lower_bound(globalTree.begin(), globalTree.end(), boundaries[rank]);
+        auto globEnd   = std::lower_bound(globalTree.begin(), globalTree.end(), boundaries[rank + 1]);
+
+        auto focStart = std::lower_bound(focusTree.begin(), focusTree.end(), boundaries[rank]);
+        auto focEnd   = std::upper_bound(focusTree.begin(), focusTree.end(), boundaries[rank + 1]) - 1;
+        if (focEnd < focStart) { focEnd = focStart; }
+
+        if (focEnd - focStart > globEnd - globStart) { peerFlags[rank] = 1; }
+        else { peerFlags[rank] = not std::includes(globStart, globEnd, focStart, focEnd); }
+    }
+    return peerFlags;
+}
+
+template<class KeyType>
+std::vector<int> oneSidedPeers(std::span<const KeyType> boundaries,
+                               int numRanks,
+                               int myRank,
+                               std::span<const KeyType> globalTree,
+                               std::span<const KeyType> focusTree)
+{
+    auto peerFlags = oneSidedPeerFlags(boundaries, numRanks, myRank, globalTree, focusTree);
+    std::vector<int> ret;
+    for (int rank = 0; rank < numRanks; ++rank)
+    {
+        if (peerFlags[rank]) { ret.push_back(rank); }
+    }
+    return ret;
+}
+
 } // namespace cstone