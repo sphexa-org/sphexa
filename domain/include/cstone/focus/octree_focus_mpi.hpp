4c6196607f18abf80c698ae47a938f49c03a90bd
Sebastian Keller <sebastian.f.keller@gmail.com>
2025-08-27 17:11:16 +0200

    add missing LET leaf-dl in find_peers

--- domain/include/cstone/focus/octree_focus_mpi.hpp
+++ domain/include/cstone/focus/octree_focus_mpi.hpp
@@ -711,6 +711,7 @@ private:
             globalTreeBackingBuffer.resize(globalLeaves.size());
             memcpyD2H(globalLeaves.data(), globalLeaves.size(), globalTreeBackingBuffer.data());
             globalLeaves = std::span(globalTreeBackingBuffer);
+            downloadOctree();
         }
 
         auto extPeers =
