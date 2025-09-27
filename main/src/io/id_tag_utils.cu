e2bc6ada3fb89ef1b3a6820264422d31f916bd70
ChristoperBignamini <christopher.bignamini@gmail.com>
2025-09-27 10:02:01 +0200

    CPU-like algorithm implemented in GPU version and code cleanup

--- main/src/io/id_tag_utils.cu
+++ main/src/io/id_tag_utils.cu
@@ -30,91 +30,39 @@
  * @author Sebastian Keller <sebastian.f.keller@gmail.com>
  */
 
-
 //TODO: check headers
+#include <thrust/copy.h>
+#include <thrust/count.h>
 #include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
-#include <thrust/sequence.h>
-#include <thrust/functional.h>
 #include <thrust/host_vector.h>
+#include <thrust/iterator/counting_iterator.h>
 #include <thrust/scan.h>
 #include <thrust/scatter.h>
 #include <thrust/transform.h>
-
-#include <thrust/count.h>
-#include <thrust/copy.h>
-#include <thrust/iterator/counting_iterator.h>
 
 #include "id_tag_utils.hpp"
 
 namespace sphexa
 {
 
-// TODO: the following two functions provides two different implementations of the tagged id identification.
-// The first one uses a prefix scan + scatter based algorithm, while the second one just uses a copy_if step:
-// according to some performace tests, namely the turbulence case of SPH-RUN with 1000^3
-// particles and a synthetic data case with 1B ids and 10% tagged ids (see find_tagged_ids_test.cpp),
-// the second one is ~2.6x/2.8x faster
-
+// TODO: to be removed, used to select between two implementations
 #if 0
-/*! @brief Tagged id identification
- *
- * @param[in]  ids              ordered id list
- * @param[in]  first            first id index // TODO number of elements and pass iterator?
- * @param[in]  last             last (excluded) id index
- * @param[out] taggedIdsIndexes vector of indexes (positions wrt of provided ids list)
- */
-void findTaggedIdsGPU(std::span<const IdType> ids, size_t first, size_t last, std::vector<LocalIndex>& taggedIdsIndexes)
-{
-    const auto devIdSize = last - first;
-    thrust::device_vector<IdType> devMask(devIdSize);
-    thrust::device_vector<IdType> devScanResult(devIdSize);
 
-    // Generate mask
-    thrust::transform(ids.data() + first, ids.data() + last, devMask.begin(), IsMasked{});
-
-    // Run scan
-    thrust::exclusive_scan(devMask.begin(), devMask.end(), devScanResult.begin());
-
-    if(devScanResult.back() > 0 || devMask.back() == 1)
-    {
-        // Scatter the tagged ids positions
-        thrust::device_vector<IdType> devSubsetPos(devScanResult.back());
-        if(devMask.back() == 1){
-            devSubsetPos.resize(devScanResult.back()+1);
-        }
-        thrust::device_vector<IdType> devSequence(devIdSize);
-        thrust::sequence(thrust::device, devSequence.begin(), devSequence.end(), first);
-        thrust::scatter_if(thrust::device, devSequence.begin(), devSequence.end(), devScanResult.begin(), devMask.begin(), devSubsetPos.begin());
-
-        // Copy result to host
-        taggedIdsIndexes.resize(devSubsetPos.size());
-        thrust::copy(devSubsetPos.begin(), devSubsetPos.end(), taggedIdsIndexes.begin());
-    }
-    else
-    {
-        taggedIdsIndexes.clear();
-    }
-
-    return;
-}
-#else
-
-// TODO: move to hpp in case of unification with CPU version
 struct IsMaskedGPU
 {
     const IdType* ids_ptr;
 
-    __host__ __device__
     IsMaskedGPU(const IdType* ids_ptr_) : ids_ptr(ids_ptr_) {}
 
-    __device__
-    bool operator()(LocalIndex idx) const
+    HOST_DEVICE_FUN
+    uint8_t operator()(LocalIndex idx) const
     {
-        return IsMasked{}(ids_ptr[idx]);
+       return IsMasked{}(ids_ptr[idx]);
     }
 };
 
+// TODO: this implementation is 3x faster than the one below
 /*! @brief Tagged id identification
  *
  * @param[in]  ids              ordered id list
@@ -133,12 +81,10 @@ void findTaggedIdsGPU(std::span<const IdTypeP> ids, size_t first, size_t last, s
     const size_t nTaggedIds = thrust::count_if(thrust::device, begin, end, isMasked);
 
     // Save indexes of tagged ids
-    thrust::device_vector<IdType> deviceTaggedIdsIndexes(last - first);
-    deviceTaggedIdsIndexes.resize(nTaggedIds);
+    thrust::device_vector<IdType> deviceTaggedIdsIndexes(nTaggedIds);
     thrust::copy_if(thrust::device, begin, end, deviceTaggedIdsIndexes.begin(), isMasked);
 
     // Copy indices of tagged ids to host vector
-    taggedIdsIndexes.clear();
     taggedIdsIndexes.resize(nTaggedIds);
     thrust::copy(deviceTaggedIdsIndexes.begin(), deviceTaggedIdsIndexes.end(), taggedIdsIndexes.begin());
 
@@ -146,7 +92,38 @@ void findTaggedIdsGPU(std::span<const IdTypeP> ids, size_t first, size_t last, s
 
 }
 
-template void findTaggedIdsGPU<IdType, LocalIndex>(std::span<const IdType> ids, size_t first, size_t last, std::vector<LocalIndex>& taggedIdsIndexes);
+#else
+
+/*! @brief Tagged id identification
+ *
+ * @param[in]  ids              ordered id list
+ * @param[in]  first            first id index // TODO number of elements and pass iterator?
+ * @param[in]  last             last (excluded) id index
+ * @param[out] taggedIdsIndexes vector of indexes (positions wrt of provided ids list)
+ */
+template<class IdTypeP, class LocalIndexP>
+void findTaggedIdsGPU(std::span<const IdTypeP> ids, size_t first, size_t last, std::vector<LocalIndexP>& taggedIdsIndexes)
+{
+
+    const auto numIds = last - first;
+    thrust::device_vector<uint8_t> flags(numIds, 0);
+
+    IsMasked isMasked;
+    thrust::transform(thrust::device, ids.data() + first, ids.data() + last, flags.begin(), isMasked);
+
+    thrust::device_vector<LocalIndexP> flagsScan(numIds);
+    thrust::exclusive_scan(flags.begin(), flags.end(), flagsScan.begin(), 0, thrust::plus<LocalIndexP>());
+    taggedIdsIndexes.resize(flagsScan.back() + flags.back());
+
+    thrust::device_vector<LocalIndexP> taggedIdsIndexesDev(taggedIdsIndexes.size());
+    thrust::scatter_if(thrust::device, thrust::make_counting_iterator(first), thrust::make_counting_iterator(first + numIds),
+            flagsScan.begin(), flags.begin(), taggedIdsIndexesDev.begin());
+    thrust::copy(taggedIdsIndexesDev.begin(), taggedIdsIndexesDev.end(), taggedIdsIndexes.begin());
+
+}
 
 #endif
+
+template void findTaggedIdsGPU<IdType, LocalIndex>(std::span<const IdType> ids, size_t first, size_t last, std::vector<LocalIndex>& taggedIdsIndexes);
+
 }
