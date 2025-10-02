ac8ec8aa983aed9da8ab33e39f0a8c6ab8a33d3e
ChristoperBignamini <christopher.bignamini@gmail.com>
2025-10-02 11:13:11 +0200

    Group Id specification added for multiple id selection support

--- main/src/io/id_tag_utils.cpp
+++ main/src/io/id_tag_utils.cpp
@@ -33,74 +33,70 @@
 #include <omp.h>
 
 #include <algorithm>
-#include <execution>
+//#include <execution>
+#include <iostream>
 
 #include "id_tag_utils.hpp"
 
 namespace sphexa
 {
 
-/*! @brief Id tagging (in first:last range) from list, CPU version
- *
- * @param[out] ids          ordered id list
- * @param[in]  first        first id index // TODO number of elements and pass iterator?
- * @param[in]  last         last (excluded) id index
- * @param[in]  selectedIds  indexes to be tagged
- */
+void applyTaggingMask(IdType groupId, IdType& id)
+{
+    if (groupId >= supGroupId)
+        throw std::runtime_error("Tagging group id larger than max value (" + std::to_string(supGroupId) + ")\n");
+
+    // Clear previous tagging if present
+    if(IsMasked{}(id)) {
+        id &= ~taggingCheckMask;
+        std::cout << "Warning: applying tagging mask to already tagged id " << id << "\n";
+    }
+
+    // Apply new tagging information
+    id |= ((groupId+1) << taggingMaskStartingBit);
+}
+
 template<class IdTypeP>
-void tagIdsInList(std::span<IdTypeP> ids, size_t first, size_t last, std::span<const IdTypeP> selectedIds)
+void tagIdsInList(std::span<IdTypeP> ids, size_t first, size_t last, std::span<const IdTypeP> selectedIds,
+    const LocalIndex groupId)
 {
     const auto idListBeginIt = ids.begin()+first;
     const auto idListEndIt = ids.begin()+last;
     auto lastFound = 0;
-    std::for_each(selectedIds.begin(), selectedIds.end(), [idListBeginIt, idListEndIt, &lastFound](auto selectedIds){
+    std::for_each(selectedIds.begin(), selectedIds.end(), [idListBeginIt, idListEndIt, &lastFound, groupId](auto selectedIds){
         auto lower = std::lower_bound(idListBeginIt+lastFound, idListEndIt, selectedIds);
         if(lower != idListEndIt && *lower == selectedIds) {
             lastFound = lower - idListBeginIt + 1;
-            *lower = *lower | msbMask;
+            applyTaggingMask(groupId, *lower);
         }
     });
 }
-template void tagIdsInList<IdType>(std::span<IdType> ids, size_t first, size_t last, std::span<const IdType> selectedIds);
+template void tagIdsInList<IdType>(std::span<IdType> ids, size_t first, size_t last, std::span<const IdType> selectedIds,
+    const LocalIndex groupId);
 
 
-// TODO: should we save here the list of tagged ids without tag?
-/*! @brief Id tagging (in first:last range) in spherical volume, CPU version
- *
- * @param[out] ids                ordered id list
- * @param[in]  x                  x coordinates
- * @param[in]  y                  y coordinates
- * @param[in]  z                  z coordinates
- * @param[in]  first              first id index // TODO number of elements and pass iterator?
- * @param[in]  last               last (excluded) id index
- * @param[in]  selSphereData      spherical volume definition
- */
 template<class IdTypeP>
 void tagIdsInSphere(std::span<IdTypeP> ids, std::span<const CoordinateType> x, std::span<const CoordinateType> y,
-    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, const IdSelectionSphere& selSphereData)
+    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, IdSelectionSphere selSphereData,
+    const LocalIndex groupId)
 {
-    const auto squareRadius = selSphereData.radius*selSphereData.radius;
+    const auto squareRadius = selSphereData[3]*selSphereData[3];
+    const auto sphereCenter = util::makeVec3(selSphereData);
 #pragma omp parallel for schedule(static)
     for(auto particleIndex = firstIndex; particleIndex < lastIndex; particleIndex++){
         cstone::Vec3<CoordinateType> currentPosition{x[particleIndex], y[particleIndex], z[particleIndex]};
-        auto squaredDistance = util::norm2(currentPosition - selSphereData.center);
+        auto squaredDistance = util::norm2(currentPosition - sphereCenter);
         if(squaredDistance < squareRadius) {
-            ids[particleIndex] = ids[particleIndex] | msbMask;
+            applyTaggingMask(groupId, ids[particleIndex]);
         }
     }
 }
 template void tagIdsInSphere<IdType>(std::span<IdType> ids, std::span<const CoordinateType> x, std::span<const CoordinateType> y,
-    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, const IdSelectionSphere& selSphereData);
+    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, IdSelectionSphere selSphereData,
+    const LocalIndex groupId);
 
 #if 1
 
-/*! @brief Tagged id identification
- *
- * @param[in]  ids              ordered id list
- * @param[in]  first            first id index // TODO number of elements and pass iterator?
- * @param[in]  last             last (excluded) id index
- * @param[out] taggedIdsIndexes vector of indexes (positions wrt of provided ids list)
- */
 template<class IdTypeP, class LocalIndexP>
 void findTaggedIds(std::span<const IdTypeP> ids, size_t first, size_t last, std::vector<LocalIndexP>& taggedIdsIndexes)
 {
@@ -125,13 +121,7 @@ void findTaggedIds(std::span<const IdTypeP> ids, size_t first, size_t last, std:
 }
 
 #else
-/*! @brief Tagged id identification
- *
- * @param[in]  ids              ordered id list
- * @param[in]  first            first id index // TODO number of elements and pass iterator?
- * @param[in]  last             last (excluded) id index
- * @param[out] taggedIdsIndexes vector of indexes (positions wrt of provided ids list)
- */
+
 template<class IdTypeP, class LocalIndexP>
 void findTaggedIds(std::span<const IdTypeP> ids, size_t first, size_t last, std::vector<LocalIndexP>& taggedIdsIndexes)
 {
--- main/src/io/id_tag_utils.hpp
+++ main/src/io/id_tag_utils.hpp
@@ -46,9 +46,20 @@ using IdType = uint64_t;
 using LocalIndex = uint32_t;
 using CoordinateType = sph::SphTypes::CoordinateType;
 
-/*! @brief Tagging mask definition (most significant bit flip)
+/*! @brief Number of bits used for tagging information storage
  */
-constexpr IdType msbMask = static_cast<IdType>(1) << (sizeof(IdType)*8 - 1);
+constexpr IdType taggingMaskSize = 10;
+
+/*! @brief Given the taggingMaskSize, this is the maximum selection group id value plus one
+ */
+constexpr IdType supGroupId = (1 << taggingMaskSize) - 1;
+
+/*! @brief First tagging bit position
+ */
+constexpr IdType taggingMaskStartingBit = sizeof(IdType)*8 - taggingMaskSize;
+
+constexpr IdType taggingCheckMask = supGroupId << taggingMaskStartingBit;
+
 
 /*! @brief Tagged id identification condition functor
  */
@@ -56,9 +67,16 @@ struct IsMasked
 {
     HOST_DEVICE_FUN uint8_t operator()(IdType id) const
     {
-        return ((id & msbMask) != 0) ? 1 : 0;
+        return (id & taggingCheckMask) != 0;
     }
 };
+
+/*! @brief Application of tagging mask to a given id
+ *
+ * @param[in]  groupId      selection group id (must be < supGroupId)
+ * @param[out] id           input id with tagging mask applied
+ */
+void applyTaggingMask(IdType groupId, IdType& id);
 
 /*! @brief Tagged id (in first:last range) identification, CPU version
  *
@@ -82,30 +100,28 @@ template<class IdTypeP, class LocalIndexP>
 void findTaggedIdsGPU(std::span<const IdTypeP> ids, size_t first, size_t last, std::vector<LocalIndexP>& taggedIdsIndexes);
 extern template void findTaggedIdsGPU<IdType, LocalIndex>(std::span<const IdType> ids, size_t first, size_t last, std::vector<LocalIndex>& taggedIdsIndexes);
 
+
+/*! @brief Id tagging spherical volume definition
+ * (center[0:2], radius)
+ */
+using IdSelectionSphere = cstone::Vec4<CoordinateType>;
+
+/*! @brief Id tagging list definition
+ */
+using IdSelectionList = std::vector<IdType>;
+
 /*! @brief Id tagging (in first:last range) from list, CPU version
  *
  * @param[out] ids               id list
  * @param[in]  first             first id index // TODO number of elements and pass iterator?
  * @param[in]  last              last (excluded) id index
  * @param[in]  selectedIds       indexes to be tagged
+ * @param[in]  groupId           selection group id
  */
 template<class IdTypeP>
-void tagIdsInList(std::span<IdTypeP> ids, size_t first, size_t last, std::span<const IdTypeP> selectedIds);
-extern template void tagIdsInList<IdType>(std::span<IdType> ids, size_t first, size_t last, std::span<const IdType> selectedIds);
-
-// Id tagging types selection
-/*! @brief Id tagging spherical volume definition
- */
-// TODO: selection spheres can be stored as cstone::Vec4<Tc>
-struct IdSelectionSphere
-{
-    cstone::Vec3<CoordinateType> center;
-    CoordinateType radius;
-};
-/*! @brief Id tagging list definition
- */
-using IdSelectionList = std::vector<IdType>;
-
+void tagIdsInList(std::span<IdTypeP> ids, size_t first, size_t last, std::span<const IdTypeP> selectedIds, const LocalIndex groupId = 0);
+extern template void tagIdsInList<IdType>(std::span<IdType> ids, size_t first, size_t last, std::span<const IdType> selectedIds,
+    const LocalIndex groupId);
 
 /*! @brief Id tagging (in first:last range) in spherical volume, CPU version
  *
@@ -116,10 +132,13 @@ using IdSelectionList = std::vector<IdType>;
  * @param[in]  first             first id index // TODO number of elements and pass iterator?
  * @param[in]  last              last (excluded) id index
  * @param[in]  selSphereData     spherical volume definition
+ * @param[in]  groupId           selection group id
  */
 template<class IdTypeP>
 void tagIdsInSphere(std::span<IdTypeP> ids, std::span<const CoordinateType> x, std::span<const CoordinateType> y,
-    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, const IdSelectionSphere& selSphereData);
+    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, IdSelectionSphere selSphereData,
+    const LocalIndex groupId = 0);
 extern template void tagIdsInSphere<IdType>(std::span<IdType> ids, std::span<const CoordinateType> x, std::span<const CoordinateType> y,
-    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, const IdSelectionSphere& selSphereData);
+    std::span<const CoordinateType> z, size_t firstIndex, size_t lastIndex, IdSelectionSphere selSphereData,
+    const LocalIndex groupId);
 }
