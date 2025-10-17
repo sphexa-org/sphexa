7b2faa0c700aa4853cea2cbb4fc49b96e0c89558
OsmanSeckinSimsek <osman.simsek@unibas.ch>
2025-10-17 10:24:22 +0200

    add star_relaxation propagator with bdt and non-bdt versions to support gpu and cpu testing

---
+++ main/src/propagator/star_relaxation_bdt.cpp
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 CSCS, ETH Zurich
+ *               2024 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! @file
+ * @brief Translation unit for the star relaxation bdt propagator initializer
+ *
+ * @author Osman Seckin Simsek <osman.simsek@unibas.ch>
+ */
+
+#include "sph/types.hpp"
+#include "propagator.h"
+#include "star_relaxation_bdt.hpp"
+
+namespace sphexa
+{
+
+template<class DomainType, class ParticleDataType>
+std::unique_ptr<Propagator<DomainType, ParticleDataType>>
+PropLib<DomainType, ParticleDataType>::makeStarRelaxationBDTProp(std::ostream& output, size_t rank,
+                                                          const InitSettings& settings, bool avClean)
+{
+    if (avClean)
+    {
+        return std::make_unique<StarRelaxationBDTProp<true, DomainType, ParticleDataType>>(output, rank, settings);
+    }
+    else { return std::make_unique<StarRelaxationBDTProp<false, DomainType, ParticleDataType>>(output, rank, settings); }
+}
+
+#ifdef USE_CUDA
+template struct PropLib<cstone::Domain<SphTypes::KeyType, SphTypes::CoordinateType, cstone::GpuTag>,
+                        SimulationData<cstone::GpuTag>>;
+#else
+template struct PropLib<cstone::Domain<SphTypes::KeyType, SphTypes::CoordinateType, cstone::CpuTag>,
+                        SimulationData<cstone::CpuTag>>;
+#endif
+
+} // namespace sphexa
