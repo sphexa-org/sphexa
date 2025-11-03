4065e01a970ddf55a8d341e9de83900131b5af7a
Felix Thaler <thaler@cscs.ch>
2025-11-03 11:12:18 +0100

    Clean up collisions.hpp

--- domain/include/cstone/traversal/collisions.hpp
+++ domain/include/cstone/traversal/collisions.hpp
@@ -35,42 +35,23 @@ HOST_DEVICE_FUN void findCollisions(const KeyType* nodePrefixes,
                                     KeyType excludeEnd,
                                     uint8_t* flags)
 {
-    constexpr T epsilon = std::numeric_limits<T>::epsilon();
-    if (std::abs(targetSize[0]) <= epsilon && std::abs(targetSize[1]) <= epsilon && std::abs(targetSize[2]) <= epsilon)
+    if (targetSize[0] == 0 && targetSize[1] == 0 && targetSize[2] == 0)
     {
         // if the target is empty, we return no overlap
-        std::cout << "[findCollisions] empty target -> no overlap" << std::endl;
         return;
     }
     auto overlaps = [&](TreeNodeIndex idx)
     {
         auto [nk1, nk2] = decodePlaceholderBit2K(nodePrefixes[idx]);
-        // std::cout << "[findCollisions] idx: " << idx << " nodeKey: " << std::oct << nk1 << ", " << nk2 << std::dec << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " nodeCenters: " << nodeCenters[idx][0] << ", " << nodeCenters[idx][1] << ", " << nodeCenters[idx][2] << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " nodeSizes: " << nodeSizes[idx][0] << ", " << nodeSizes[idx][1] << ", " << nodeSizes[idx][2] << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " targetCenter: " << targetCenter[0] << ", " << targetCenter[1] << ", " << targetCenter[2] << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " targetSize: " << targetSize[0] << ", " << targetSize[1] << ", " << targetSize[2] << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " !containedIn(nk1, nk2, excludeStart, excludeEnd): " << !containedIn(nk1, nk2, excludeStart, excludeEnd) << std::endl;
-        // std::cout << "[findCollisions] idx: " << idx << " overlap: " << overlap(nodeCenters[idx], nodeSizes[idx], targetCenter, targetSize, box) << std::endl;
-        constexpr T epsilon = std::numeric_limits<T>::epsilon();
-        if (std::abs(nodeSizes[idx][0]) <= epsilon && std::abs(nodeSizes[idx][1]) <= epsilon && std::abs(nodeSizes[idx][2]) <= epsilon)
+        if (nodeSizes[idx][0] == 0 && nodeSizes[idx][1] == 0 && nodeSizes[idx][2] == 0)
         {
             // if the cell is empty, we return no overlap
-            // std::cout << "[findCollisions] idx: " << idx << " empty cell -> no overlap" << std::endl;
             return false;
         }
-        // return false;
 
-        bool bOverlap   = !containedIn(nk1, nk2, excludeStart, excludeEnd) &&
+        bool bOverlap = !containedIn(nk1, nk2, excludeStart, excludeEnd) &&
                         overlap(nodeCenters[idx], nodeSizes[idx], targetCenter, targetSize, box);
-        if (bOverlap) {
-            // std::cout << "idx " << idx << " box [ " << nodeCenters[idx][0] - nodeSizes[idx][0] << ", " << nodeCenters[idx][1] - nodeSizes[idx][1] << ", " << nodeCenters[idx][2] - nodeSizes[idx][2]
-            //           << "] - [" << nodeCenters[idx][0] + nodeSizes[idx][0] << ", " << nodeCenters[idx][1] + nodeSizes[idx][1] << ", " << nodeCenters[idx][2] + nodeSizes[idx][2]
-            //           << "] overlaps with target box [ " << targetCenter[0] - targetSize[0] << ", " << targetCenter[1] - targetSize[1] << ", " << targetCenter[2] - targetSize[2]
-            //           << "] - [" << targetCenter[0] + targetSize[0] << ", " << targetCenter[1] + targetSize[1] << ", " << targetCenter[2] + targetSize[2]
-            //           << "] nk1: " << std::oct << nk1 << " nk2: " << nk2 << std::dec << std::endl;
-            flags[idx] = 1;
-        }
+        if (bOverlap) { flags[idx] = 1; }
         return bOverlap;
     };
 
@@ -113,31 +94,27 @@ void findHalos(const KeyType* prefixes,
     KeyType highestKey = leaves[lastNode];
 
     const auto mixDBits = getBoxMixDimensionBits<Tc, KeyType, Box<Tc>>(box);
-    const bool use_mixD = mixDBits.bx != maxTreeLevel<KeyType>{} ||
-                    mixDBits.by != maxTreeLevel<KeyType>{} ||
-                    mixDBits.bz != maxTreeLevel<KeyType>{};
-    // if (!use_mixD) {
-    //     throw std::runtime_error("findHalos: non-mixD case not implemented");
-    // }
-
+    const bool use_mixD = mixDBits.bx != maxTreeLevel<KeyType>{} || mixDBits.by != maxTreeLevel<KeyType>{} ||
+                          mixDBits.bz != maxTreeLevel<KeyType>{};
 
 #pragma omp parallel for
     for (TreeNodeIndex leafIdx = firstNode; leafIdx < lastNode; ++leafIdx)
     {
         constexpr Tc epsilon = std::numeric_limits<Tc>::epsilon();
-        if (std::abs(searchSizes[leafIdx][0]) <= epsilon &&
-            std::abs(searchSizes[leafIdx][1]) <= epsilon &&
-            std::abs(searchSizes[leafIdx][2]) <= epsilon)
+        if (searchSizes[leafIdx][0] == 0 && searchSizes[leafIdx][1] == 0 && searchSizes[leafIdx][2] == 0)
         {
             // if the target is empty, we skip it
-            // std::cout << "[findHalos] Skipping leafIdx: " << leafIdx << std::endl;
             continue;
         }
-        // std::cout << "[findHalos] leafIdx: " << leafIdx << " searchCenter: " << searchCenters[leafIdx][0] << ", " << searchCenters[leafIdx][1] << ", " << searchCenters[leafIdx][2] << std::endl;
-        // std::cout << "[findHalos] leafIdx: " << leafIdx << " searchSize: " << searchSizes[leafIdx][0] << ", " << searchSizes[leafIdx][1] << ", " << searchSizes[leafIdx][2] << std::endl;
-        // if the halo box is fully inside the assigned SFC range, we skip collision detection
-        if (use_mixD && containedIn(lowestKey, highestKey, searchCenters[leafIdx], searchSizes[leafIdx], box, mixDBits.bx, mixDBits.by, mixDBits.bz)) { /*std::cout << "[findHalos] contained in TRUE" << std::endl;*/ continue; }
-        if (!use_mixD && containedIn(lowestKey, highestKey, searchCenters[leafIdx], searchSizes[leafIdx], box)) { continue; }
+        if (use_mixD && containedIn(lowestKey, highestKey, searchCenters[leafIdx], searchSizes[leafIdx], box,
+                                    mixDBits.bx, mixDBits.by, mixDBits.bz))
+        {
+            continue;
+        }
+        if (!use_mixD && containedIn(lowestKey, highestKey, searchCenters[leafIdx], searchSizes[leafIdx], box))
+        {
+            continue;
+        }
         findCollisions(prefixes, childOffsets, parents, nodeCenters, nodeSizes, searchCenters[leafIdx],
                        searchSizes[leafIdx], box, lowestKey, highestKey, collisionFlags);
     }
