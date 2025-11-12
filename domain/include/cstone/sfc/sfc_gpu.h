c624b5e14d38b0dc26d0e9de7f7eabf8007ebae2
Ioannis Magkanaris <iomagkanaris@gmail.com>
2025-11-12 17:16:08 +0100

    Added --expt-relaxed-constexpr to compile without warnings iHilbertMixD with std functions and added computation of SFC keys to the GPU

--- domain/include/cstone/sfc/sfc_gpu.h
+++ domain/include/cstone/sfc/sfc_gpu.h
@@ -23,6 +23,9 @@ namespace cstone
 template<class KeyType, class T>
 extern void computeSfcKeysGpu(const T* x, const T* y, const T* z, KeyType* keys, size_t numKeys, const Box<T>& box);
 
+template<class KeyType, class T>
+extern void computeSfcMixDKeysGpu(const T* x, const T* y, const T* z, KeyType* keys, size_t numKeys, const Box<T>& box, unsigned bx, unsigned by, unsigned bz);
+
 template<bool useGpu, class KeyType, class T>
 void computeSfcKeys(const T* x, const T* y, const T* z, KeyType* keys, size_t numKeys, const Box<T>& box)
 {
@@ -33,10 +36,8 @@ void computeSfcKeys(const T* x, const T* y, const T* z, KeyType* keys, size_t nu
 template<bool useGpu, class KeyType, class T>
 void computeSfcMixDKeys(const T* x, const T* y, const T* z, KeyType* keys, size_t numKeys, const Box<T>& box, unsigned bx, unsigned by, unsigned bz)
 {
-    // if constexpr (useGpu) { computeSfcMixDKeysGpu(x, y, z, keys, numKeys, box, bx, by, bz); }
-    // else { computeSfcMixDKeys(x, y, z, keys, numKeys, box, bx, by, bz); }
-    // TODO(iomaganaris): for now only CPU version
-    computeSfcMixDKeys(x, y, z, keys, numKeys, box, bx, by, bz);
+    if constexpr (useGpu) { computeSfcMixDKeysGpu(x, y, z, keys, numKeys, box, bx, by, bz); }
+    else { computeSfcMixDKeys(x, y, z, keys, numKeys, box, bx, by, bz); }
 }
 
 
