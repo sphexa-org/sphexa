207ca7e82ebd170d4406fca4b0e39366893475a5
Sebastian Keller <sebastian.f.keller@gmail.com>
2025-11-28 17:00:38 +0100

    restore dropped relaxSystemKernel

--- sph/include/sph/hydro_std/momentum_energy_gpu.cu
+++ sph/include/sph/hydro_std/momentum_energy_gpu.cu
@@ -173,4 +173,35 @@ void computeMomentumEnergyStdGpu(const GroupView& grp, Dataset& d, const cstone:
 
 template void computeMomentumEnergyStdGpu(const GroupView& grp, sphexa::ParticlesData<cstone::GpuTag>& d,
                                           const cstone::Box<SphTypes::CoordinateType>&);
+
+template<typename Thydro, typename T>
+__global__ void relaxSystemKernel(size_t first, size_t last, Thydro* ax, Thydro* ay, Thydro* az, Thydro* vx, Thydro* vy,
+                                  Thydro* vz, T relaxationTimescale)
+{
+    cstone::LocalIndex i = first + blockDim.x * blockIdx.x + threadIdx.x;
+    if (i >= last) { return; }
+
+    ax[i] -= vx[i] / relaxationTimescale;
+    ay[i] -= vy[i] / relaxationTimescale;
+    az[i] -= vz[i] / relaxationTimescale;
+}
+
+template<typename Thydro, typename T>
+void relaxSystemGPU(size_t first, size_t last, Thydro* ax, Thydro* ay, Thydro* az, Thydro* vx, Thydro* vy, Thydro* vz,
+                    T relaxationTimescale)
+{
+    cstone::LocalIndex numParticles = last - first;
+    unsigned           numThreads   = 256;
+    unsigned           numBlocks    = (numParticles + numThreads - 1) / numThreads;
+
+    relaxSystemKernel<<<numBlocks, numThreads>>>(first, last, ax, ay, az, vx, vy, vz, relaxationTimescale);
+    checkGpuErrors(cudaDeviceSynchronize());
+}
+
+#define RELAX_SYSTEM_GPU(Thydro, T)                                                                                    \
+    template void relaxSystemGPU(size_t first, size_t last, Thydro* ax, Thydro* ay, Thydro* az, Thydro* vx,            \
+                                 Thydro* vy, Thydro* vz, T relaxationTimescale);
+RELAX_SYSTEM_GPU(float, double);
+RELAX_SYSTEM_GPU(double, double);
+
 } // namespace sph
