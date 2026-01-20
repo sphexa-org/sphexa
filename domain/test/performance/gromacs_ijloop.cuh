810ed236c949982d06a6f9318f914ec7f998a921
Felix Thaler <thaler@cscs.ch>
2026-01-20 15:12:09 +0100

    Remove unused include

--- domain/test/performance/gromacs_ijloop.cuh
+++ domain/test/performance/gromacs_ijloop.cuh
@@ -28,7 +28,6 @@
 #include "cstone/reducearray.cuh"
 #include "cstone/traversal/ijloop/ijloop.hpp"
 #include "cstone/tree/octree.hpp"
-#include "cstone/util/uninitialized.hpp"
 
 namespace cstone::ijloop
 {
