73e4253c1ac4b37972b3cb0899b2f4674138c0aa
ChristoperBignamini <christopher.bignamini@gmail.com>
2026-02-03 13:06:20 +0100

    Refactoring of outputAllocatedFields and outputSubsetAllocatedFields methods

--- main/src/propagator/ipropagator.hpp
+++ main/src/propagator/ipropagator.hpp
@@ -34,8 +34,11 @@
 
 #include <variant>
 
+#include "cstone/cuda/cuda_utils.hpp"
+#include "cstone/cuda/device_vector.h"
 #include "cstone/sfc/box.hpp"
 #include "io/ifile_io.hpp"
+#include "io/id_tag_utils.hpp"
 #include "sph/particles_data.hpp"
 #include "util/pm_reader.hpp"
 #include "util/timer.hpp"
@@ -47,6 +50,10 @@ template<class DomainType, class ParticleDataType>
 class Propagator
 {
     using T = typename ParticleDataType::RealType;
+    using Acc = typename ParticleDataType::AcceleratorType;
+    template<class VType>
+    using AccVector = std::conditional_t<cstone::HaveGpu<Acc>{}, cstone::DeviceVector<VType>, std::vector<VType>>;
+
 
 public:
     Propagator(std::ostream& output, int rank)
@@ -74,6 +81,41 @@ public:
 
     //! @brief save particle data fields to file
     virtual void saveFields(IFileWriter*, size_t, size_t, ParticleDataType&, const cstone::Box<T>&) {}
+
+    // TODO: can be in the saveSubsetsFields method below if we don't need to override it in derived classes (for example in case of EOS related ouput)
+    //! @brief save particle subset data fields to file
+    virtual void saveSubFields(IFileWriter* writer, std::span<const uint64_t> selectedParticlesIndexes, ParticleDataType& simData) 
+    {
+        outputAllocatedFields(writer, simData, selectedParticlesIndexes);
+        timer.step("SubsetFileOutput");
+    }
+
+    //! @brief save selected particle data fields to file
+    // TODO: ParticleDataType should be const ParticleDataType& but at some point we use the data() method which is non-const
+    // TODO: why in saveFields a file name parameter is not present?  
+    void saveSubsetsFields(IFileWriter* writer, std::string selParticlesOutFile, size_t first, size_t last, ParticleDataType& simData)
+    {
+        // Find the selected particles positions in dataset
+        // TODO: check selectedParticlesIndexes template parameter type
+        AccVector<uint64_t> selectedParticlesIndexes;
+        if constexpr (cstone::HaveGpu<typename ParticleDataType::AcceleratorType>{}) {
+            // cstone::DeviceVector<uint64_t> selectedParticlesIndexesDev;
+            findTaggedIdsGPU(std::span<const uint64_t>(simData.hydro.id.data(), simData.hydro.id.size()), first, last, selectedParticlesIndexes);
+            // selectedParticlesIndexes.resize(selectedParticlesIndexesDev.size());
+            // size_t transferSize = selectedParticlesIndexesDev.size() * sizeof(uint64_t);
+            // memcpyH2D(rawPtr(selectedParticlesIndexesDev), transferSize, selectedParticlesIndexes.data());
+        }
+        else {
+            // TODO: why different from  findTaggedIdsGPU
+            findTaggedIds(std::span<const uint64_t>(simData.hydro.id), first, last, selectedParticlesIndexes);
+        }
+        timer.step("FindTaggedIds");
+
+        writer->addStep(0, selectedParticlesIndexes.size(), selParticlesOutFile);
+        writer->stepAttribute("iteration", &simData.hydro.iteration, 1);
+        saveSubFields(writer, selectedParticlesIndexes, simData);
+        writer->closeStep();
+    }
 
     //! @brief save internal state to file
     virtual void save(IFileWriter*) {}
@@ -130,13 +172,57 @@ public:
     }
 
 protected:
-    static void outputAllocatedFields(IFileWriter* writer, ParticleDataType& simData)
+
+    // static void outputAllocatedFields(IFileWriter* writer, ParticleDataType& simData)
+    // {
+    //     auto output = [](auto& d, IFileWriter* writer)
+    //     {
+    //         auto fieldPointers = d.data();
+    //         auto indicesDone   = d.outputFieldIndices;
+    //         auto namesDone     = d.outputFieldNames;
+
+    //         for (int i = int(indicesDone.size()) - 1; i >= 0; --i)
+    //         {
+    //             int fidx = indicesDone[i];
+    //             if (d.isAllocated(fidx))
+    //             {
+    //                 int column = std::find(d.outputFieldIndices.begin(), d.outputFieldIndices.end(), fidx) -
+    //                              d.outputFieldIndices.begin();
+    //                 std::visit(
+    //                     [writer, c = column, key = namesDone[i]](auto field)
+    //                     {
+    //                         auto&& tmp = toHost(*field);
+    //                         writeField(writer, key, tmp.data(), c);
+    //                     },
+    //                     fieldPointers[fidx]);
+    //                 indicesDone.erase(indicesDone.begin() + i);
+    //                 namesDone.erase(namesDone.begin() + i);
+    //             }
+    //         }
+
+    //         if (!indicesDone.empty() && writer->rank() == 0)
+    //         {
+    //             std::cout << "WARNING: the following fields are not in use and therefore not output: ";
+    //             for (int fidx = 0; fidx < indicesDone.size() - 1; ++fidx)
+    //             {
+    //                 std::cout << d.fieldNames[fidx] << ",";
+    //             }
+    //             std::cout << d.fieldNames[indicesDone.back()] << std::endl;
+    //         }
+    //     };
+
+    //     output(simData.hydro, writer);
+    //     output(simData.chem, writer);
+    // }
+
+    static void outputAllocatedFields(IFileWriter* writer, ParticleDataType& simData, std::optional<std::span<const uint64_t>> selectedParticlesIndexes = std::nullopt)
     {
-        auto output = [](auto& d, IFileWriter* writer)
+        auto output = [](auto& d, IFileWriter* writer, std::optional<std::span<const uint64_t>> selectedParticlesIndexes = std::nullopt)
         {
             auto fieldPointers = d.data();
             auto indicesDone   = d.outputFieldIndices;
             auto namesDone     = d.outputFieldNames;
+            AccVector<char> buffer;
 
             for (int i = int(indicesDone.size()) - 1; i >= 0; --i)
             {
@@ -145,6 +231,23 @@ protected:
                 {
                     int column = std::find(d.outputFieldIndices.begin(), d.outputFieldIndices.end(), fidx) -
                                  d.outputFieldIndices.begin();
+                    if(selectedParticlesIndexes != std::nullopt)
+                    {
+                        std::visit(
+                            [writer, c = column, key = namesDone[i], &selectedParticlesIndexes, &buffer](auto field)
+                            {
+                                const auto selIndexes = selectedParticlesIndexes.value();
+                                using ValueType = std::remove_pointer_t<decltype(field)>::value_type;
+                                auto packedBuffer = util::packAllocBuffer<ValueType>(buffer, std::vector<size_t>{selIndexes.size()}, 1);
+                                constexpr bool gpu = cstone::HaveGpu<Acc>{};
+                                cstone::gatherAcc<gpu>(selIndexes, field->data(), packedBuffer[0].data());
+                                auto&& tmp = toHost(buffer);
+                                writeField(writer, key, tmp.data(), c);
+                            },
+                            fieldPointers[fidx]);
+                    }
+                    else
+                    {
                     std::visit(
                         [writer, c = column, key = namesDone[i]](auto field)
                         {
@@ -152,6 +255,7 @@ protected:
                             writeField(writer, key, tmp.data(), c);
                         },
                         fieldPointers[fidx]);
+                    }
                     indicesDone.erase(indicesDone.begin() + i);
                     namesDone.erase(namesDone.begin() + i);
                 }
@@ -168,9 +272,57 @@ protected:
             }
         };
 
-        output(simData.hydro, writer);
-        output(simData.chem, writer);
+        output(simData.hydro, writer, selectedParticlesIndexes);
+        output(simData.chem, writer, selectedParticlesIndexes);
     }
+
+
+    // // TODO: is there any way to avoid code duplication with outputAllocatedFields()?
+    // static void outputSubsetAllocatedFields(IFileWriter* writer, std::span<const uint64_t> selectedParticlesIndexes, 
+    //     ParticleDataType& simData)
+    // {
+    //     auto output = [](auto& d, IFileWriter* writer, const std::span<const uint64_t> selectedParticlesIndexes)
+    //     {
+    //         auto indicesDone   = d.subsetOutputFieldIndices;
+    //         auto namesDone     = d.subsetOutputFieldNames;
+    //         // TODO: use AccVector for subsetField (resize/clean if needed)
+    //         // TODO: have a look to PackedBuffer and PackAlloc
+    //         for (int i = int(indicesDone.size()) - 1; i >= 0; --i)
+    //         {
+    //             int fidx = indicesDone[i];
+    //             if (d.isAllocated(fidx))
+    //             {
+    //                 int column = std::find(d.subsetOutputFieldIndices.begin(), d.subsetOutputFieldIndices.end(), fidx) -
+    //                              d.subsetOutputFieldIndices.begin();
+                    
+    //                 // TODO: passing the entire data is not needed
+    //                 using FieldVariant = std::variant<std::vector<float>, std::vector<double>, std::vector<unsigned>, std::vector<uint64_t>, std::vector<uint8_t>>;                    
+    //                 FieldVariant subsetField;
+    //                 createHostSubsetFieldDataset(d, selectedParticlesIndexes, fidx, subsetField);
+
+    //                 std::visit([writer, c = column, key = namesDone[i]](auto field)
+    //                            { writeField(writer, key, field.data(), c); }, subsetField);
+
+    //                 indicesDone.erase(indicesDone.begin() + i);
+    //                 namesDone.erase(namesDone.begin() + i);
+    //             }
+    //         }
+
+    //         if (!indicesDone.empty() && writer->rank() == 0)
+    //         {
+    //             std::cout << "WARNING: the following fields are not in use and therefore not output for subset: ";
+    //             for (int fidx = 0; fidx < indicesDone.size() - 1; ++fidx)
+    //             {
+    //                 std::cout << d.fieldNames[fidx] << ",";
+    //             }
+    //             std::cout << d.fieldNames[indicesDone.back()] << std::endl;
+    //         }
+    //     };
+
+    //     output(simData.hydro, writer, selectedParticlesIndexes);
+    //     output(simData.chem, writer, selectedParticlesIndexes);
+    // }
+
 
     void logDomainStats(const DomainType& domain, ParticleDataType& simData)
     {
