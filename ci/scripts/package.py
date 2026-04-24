1d7a7c8d3f1dd75e72452fbb39ff963c0080fcf0
jgphpc <jgp@cscs.ch>
2026-04-24 08:35:08 +0200

    Re-enable cuda@12

--- ci/scripts/package.py
+++ ci/scripts/package.py
@@ -38,7 +38,7 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("cxx", type="build")
 
     depends_on("mpi")
-    depends_on("cuda@13:", when="@0.95: +cuda")
+    depends_on("cuda@12:", when="@0.95: +cuda")
     depends_on("hip", when="+rocm")
     depends_on("rocthrust", when="+rocm")
     depends_on("hipcub", when="+rocm")
