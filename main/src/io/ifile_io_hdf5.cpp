d2730cbaa035b7a82323c8b0697abf95bc5a40f8
ChristoperBignamini <christopher.bignamini@gmail.com>
2026-04-27 10:26:44 +0200

    Zero particle output scenario added for tagging

--- main/src/io/ifile_io_hdf5.cpp
+++ main/src/io/ifile_io_hdf5.cpp
@@ -68,7 +68,7 @@ public:
 
     std::string suffix() const override { return ".h5"; }
 
-    void addStep(size_t firstIndex, size_t lastIndex, std::string path) override
+    void addStep(size_t firstIndex, size_t lastIndex, std::string path, bool settingsWriting = false) override
     {
         firstIndex_ = firstIndex;
 
@@ -78,7 +78,7 @@ public:
             h5File_ = fileutils::openH5Part(path, H5_O_RDWR, comm_);
         }
 
-        if (lastIndex > firstIndex)
+        if (lastIndex >= firstIndex && !settingsWriting)
         {
             // create the next step
             h5_int64_t numSteps = H5GetNumSteps(h5File_);
@@ -189,7 +189,7 @@ public:
         MPI_Comm_free(&comm_);
     }
 
-    void addStep(size_t firstIndex, size_t lastIndex, std::string path) override
+    void addStep(size_t firstIndex, size_t lastIndex, std::string path, bool settingsWriting = false) override
     {
         firstIndex_ = firstIndex;
         if (!h5File_ || path != pathStep_)
@@ -198,7 +198,7 @@ public:
             if (rank_ == 0) { h5File_ = fileutils::openH5Part(path, H5_O_RDWR, comm_); }
         }
 
-        if (lastIndex > firstIndex)
+        if (lastIndex >= firstIndex && !settingsWriting)
         {
             // create the next step
             if (rank_ == 0)
