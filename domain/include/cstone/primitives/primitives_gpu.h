0d9471567d88607c90033c172739d859ed955930
Felix Thaler <thaler@cscs.ch>
2026-06-24 07:48:34 +0200

    Fix missing include

--- domain/include/cstone/primitives/primitives_gpu.h
+++ domain/include/cstone/primitives/primitives_gpu.h
@@ -18,6 +18,7 @@
 #include <span>
 #include <tuple>
 
+#include "cstone/cuda/cuda_stubs.h"
 #include "cstone/cuda/device_vector.h"
 #include "cstone/execution.hpp"
 #include "cstone/tree/definitions.h"
