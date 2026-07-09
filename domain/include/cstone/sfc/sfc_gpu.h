020b6d77f89991d05afd2abc6435c4fa6c4e2956
Sebastian Keller <sebastian.f.keller@gmail.com>
2026-07-09 15:53:27 +0200

    move docstring to header

--- domain/include/cstone/sfc/sfc_gpu.h
+++ domain/include/cstone/sfc/sfc_gpu.h
@@ -21,6 +21,18 @@
 namespace cstone
 {
 
+/*! @brief CUDA kernel to compute SFC (Hilbert/Morton) keys from integer coordinates
+ *
+ * @tparam     KeyType    32-bit or 64-bit SFC key type
+ * @tparam     T          floating-point type for coordinates
+ * @param[in]  exec       CUDA stream to launch kernel on
+ * @param[in]  x          input integer x-coordinates
+ * @param[in]  y          input integer y-coordinates
+ * @param[in]  z          input integer z-coordinates
+ * @param[out] keys       output array of SFC keys
+ * @param[in]  numKeys    number of keys to compute
+ * @param[in]  box        bounding box for the coordinates
+ */
 template<class KeyType, class T>
 extern void computeSfcKeys(
     execution::Gpu exec, const T* x, const T* y, const T* z, KeyType* keys, size_t numKeys, const Box<T>& box);
