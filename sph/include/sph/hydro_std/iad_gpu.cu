d497ec329ab0f05adbb52255b3d9242b50798600
Sebastian Keller <sebastian.f.keller@gmail.com>
2026-07-15 18:21:35 +0200

    formatting

--- sph/include/sph/hydro_std/iad_gpu.cu
+++ sph/include/sph/hydro_std/iad_gpu.cu
@@ -42,9 +42,8 @@ using cstone::GroupView;
 template<class Dataset>
 void computeIADGpu(const GroupView&, Dataset& d, const cstone::Box<typename Dataset::RealType>&)
 {
-    IADIjLoop(d.neighborhood, d.K, d.iadConditionQuality, rawPtr(d.m), rawPtr(d.rho), rawPtr(d.nc), rawPtr(d.wh), rawPtr(d.c11), rawPtr(d.c12),
-              rawPtr(d.c13), rawPtr(d.c22), rawPtr(d.c23), rawPtr(d.c33),
-              rawPtr(d.id));
+    IADIjLoop(d.neighborhood, d.K, d.iadConditionQuality, rawPtr(d.m), rawPtr(d.rho), rawPtr(d.nc), rawPtr(d.wh),
+              rawPtr(d.c11), rawPtr(d.c12), rawPtr(d.c13), rawPtr(d.c22), rawPtr(d.c23), rawPtr(d.c33), rawPtr(d.id));
     checkGpuErrors(cudaDeviceSynchronize());
 }
 
