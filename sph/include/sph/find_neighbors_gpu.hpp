051ca88df92047c071e6433270a0180feff2ae84
Sebastian Keller <sebastian.f.keller@gmail.com>
2026-07-17 12:40:31 +0200

    put update-h into find_neighbors

--- sph/include/sph/find_neighbors_gpu.hpp
+++ sph/include/sph/find_neighbors_gpu.hpp
@@ -11,4 +11,10 @@ namespace sph
 void findNeighborsSfc(const cstone::GroupView& groups, sphexa::ParticlesData<cstone::execution::Gpu>& d,
                       const cstone::Box<SphTypes::CoordinateType>& box, bool subgroups = false);
 
+template<class Th, class KeyType>
+extern bool updateSmoothingLengthGpu(const cstone::GroupView&, unsigned ng0, const unsigned* nc, Th* h, KeyType* keys);
+
+template<class T, class Dataset>
+extern void updateSmoothingLengthIterativeGpu(const cstone::GroupView&, Dataset& d, const cstone::Box<T>& box);
+
 }
