54edec69d040e25c14abf9f8e0b93b1f2a75de5e
Felix Thaler <thaler@cscs.ch>
2026-08-10 14:16:19 +0200

    Revert b204754 apart from cleanup

--- domain/include/cstone/traversal/ijloop/gpu_superclusternblist/build.cuh
+++ domain/include/cstone/traversal/ijloop/gpu_superclusternblist/build.cuh
@@ -141,7 +141,7 @@ __global__ void computeJClusterBboxesKernel(const LocalIndex firstValidBody,
     if constexpr (Config::symmetric)
     {
         using Th    = std::remove_cvref_t<std::remove_pointer_t<ThP>>;
-        const Th hi = invalidHToZero(loadAtIndexIfPtr(h, std::max(std::min(i, totalBodies - 1), firstValidBody)));
+        const Th hi = loadAtIndexIfPtr(h, std::max(std::min(i, totalBodies - 1), firstValidBody));
         Th rMax     = 2 * hi;
 
 #pragma unroll
@@ -250,7 +250,7 @@ __device__ __forceinline__ auto loadSuperclusterParticleData(const LocalIndex fi
     {
         const unsigned i = std::min(firstBody + w * GpuConfig::warpSize + laneIdx, lastBody - 1);
         iPos[w]          = {x[i], y[i], z[i]};
-        iRadius[w]       = 2 * invalidHToZero(loadAtIndexIfPtr(h, i)) * searchExtFactor;
+        iRadius[w]       = 2 * loadAtIndexIfPtr(h, i) * searchExtFactor;
     }
     return std::make_tuple(iPos, iRadius);
 }
@@ -422,8 +422,7 @@ collectNeighborJClusters(const OctreeNsView<Tc, KeyType>& tree,
                     const LocalIndex j =
                         std::clamp(jCluster * Config::jSize + jClusterParticle, firstValidBody, totalBodies - 1);
                     const Vec3<Tc> jPos = {x[j], y[j], z[j]};
-                    Th jRadius =
-                        Config::symmetric ? 2 * invalidHToZero(loadAtIndexIfPtr(h, j)) * tree.searchExtFactor : Th(0);
+                    const Th jRadius    = Config::symmetric ? 2 * loadAtIndexIfPtr(h, j) * tree.searchExtFactor : Th(0);
                     const unsigned warpIndex = jClusterParticle / (Config::jSize / Config::numWarpsPerInteraction);
 
                     for (unsigned w = 0; w < warpsPerSupercluster; ++w)
