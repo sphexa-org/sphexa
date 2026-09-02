sphexa-org/sphexa

commit 5861ea269f4b3c51c3d61006995de8f83b12dcf6

New Neighborhood API (#542)
Introduces a new neighbor lists API into the corner stone library and enables its use in sphexa.

Summary of all changes:
- Introduces `cstone::ijloop` namespace which includes a new API for particle-particle neighbor search.
- Introduces the concept of neighborhoods which provide a common API for particle-particle neighbor search on CPU and GPU.
- Uses the new API for the existing full CPU neighbor list, now available as `cstone::ijloop::CpuFullNeighborListNeighborhood`.
- Uses the new API for the existing GPU neighbor list, `cstone::ijloop::GpuAlwaysTraverseNeighborhood` which wraps the existing fast warp-based tree traversal in the new API.
- Adapts the `std`, `ve`, and `ve-bdt` propagators to use the new neighborhoods.
- Introduces unit tests for the new neighborhoods.
- Note: all particle-particle distances are now computed in double precision, while previously they were computed in double precision inside the tree traversal code but re-computed in single precision in some kernels. This can lead to noticeable numerical differences in sensitive kernels (obviously with higher precision in the new implementation).

main/src/propagator/std_hydro_grackle.hpp
@@ -163,9 +163,9 @@ public:
         size_t first = domain.startIndex();
         size_t last  = domain.endIndex();
 
-        resizeNeighbors(d, domain.nParticles() * d.ngmax);
-        findNeighborsSfc(first, last, d, domain.box());
         computeGroups(first, last, d, domain.box(), groups_);
+        updateSmoothingLengthIterative(groups_.view(), d, domain.box());
+        findNeighborsSfc(groups_.view(), d, domain.box());
         timer.step("FindNeighbors");
 
         computeDensity(groups_.view(), d, domain.box());

commit 9fea2519157e02abd0659c52ff2d0a2fa201afb7

Missing dtCourant dependent field added (bug introduced in #542

main/src/propagator/std_hydro_grackle.hpp
@@ -71,7 +71,7 @@ class HydroGrackleProp final : public HydroProp<DomainType, DataType>
 
     //! @brief the list of dependent particle fields, these may be used as scratch space during domain sync
     using DependentFields =
-        FieldList<"rho", "p", "c", "ax", "ay", "az", "du", "c11", "c12", "c13", "c22", "c23", "c33", "nc">;
+        FieldList<"rho", "p", "c", "ax", "ay", "az", "du", "c11", "c12", "c13", "c22", "c23", "c33", "nc", "dtCourant">;
 
     //! @brief All fields listed in Chemistry data are used. This could be overridden with a sublist if desired
     using CoolingFields = typename cooling::Cooler<T>::CoolingFields;